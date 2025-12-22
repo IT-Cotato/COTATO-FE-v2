d4835e048c638264a8b82191d5efa15229905f18
kimminna <kmina121777@naver.com>
2025-12-22 22:31:23 +0900

    feat: 어드민 경로 상수화 (#5)

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  ADMIN: '/admin',
+  ADMIN_APPLICATION: '/admin/applications',
+  ADMIN_APPLICATION_FORM: '/admin/application-form',
+  ADMIN_RECRUITMENT: '/admin/recruitment',
+  ADMIN_RESULTS: '/admin/results',
+};
