36ae4deab2850d226929aa66f2d6c6f08d8af520
kimminna <kmina121777@naver.com>
2025-12-24 18:03:31 +0900

    feat: 어드민 지원서 열람 페이지 구현 (#5)

--- src/app/admin/(with-sidebar)/applications/page.tsx
+++ src/app/admin/(with-sidebar)/applications/page.tsx
@@ -1,3 +1,12 @@
+import {AdminApplicationInformation} from '@/app/admin/(with-sidebar)/applications/_components/AdminApplicationInformation';
+import {AdminApplicationTabs} from '@/app/admin/(with-sidebar)/applications/_components/AdminApplicationTabs';
+
 export default function AdminApplicationPage() {
-  return <div>지원서 열람</div>;
+  return (
+    <section className='flex flex-col gap-7'>
+      <h1 className='text-h4'>지원서 열람</h1>
+      <AdminApplicationInformation />
+      <AdminApplicationTabs />
+    </section>
+  );
 }
