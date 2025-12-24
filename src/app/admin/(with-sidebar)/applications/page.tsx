cb2d1476d1be8d7642eed04528cd6a923d53f12d
kimminna <kmina121777@naver.com>
2025-12-24 18:16:12 +0900

    fix(build): useSearchParams 사용을 위한 Suspense wrapper 정의 (#5)

--- src/app/admin/(with-sidebar)/applications/page.tsx
+++ src/app/admin/(with-sidebar)/applications/page.tsx
@@ -1,11 +1,14 @@
 import {AdminApplicationInformation} from '@/app/admin/(with-sidebar)/applications/_components/AdminApplicationInformation';
 import {AdminApplicationTabs} from '@/app/admin/(with-sidebar)/applications/_components/AdminApplicationTabs';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 
 export default function AdminApplicationPage() {
   return (
     <section className='flex flex-col gap-7'>
       <h1 className='text-h4'>지원서 열람</h1>
-      <AdminApplicationInformation />
+      <SuspenseWrapper>
+        <AdminApplicationInformation />
+      </SuspenseWrapper>
       <AdminApplicationTabs />
     </section>
   );
