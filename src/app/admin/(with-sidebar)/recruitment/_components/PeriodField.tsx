d57dd95f95691e471ccb8d973a686a6906d76926
choyeon <cho1428hee@naver.com>
2025-12-24 10:59:23 +0900

    chore: datepicker 라이브러리 설치 (#10)

--- src/app/admin/(with-sidebar)/recruitment/_components/PeriodField.tsx
+++ src/app/admin/(with-sidebar)/recruitment/_components/PeriodField.tsx
@@ -1,6 +1,6 @@
 export const PeriodField = () => {
   return (
-    <div className='flex w-79 flex-col gap-4'>
+    <div className='flex flex-col gap-4'>
       <label>지원기간</label>
       <div className='flex items-center gap-2.5'>{/* date picker */}</div>
     </div>
