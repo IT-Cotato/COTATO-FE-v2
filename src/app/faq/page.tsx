3bb8af924fd7ef4c4540d2bf7dfa87896ecab301
ion <delion.dev@gmail.com>
2025-12-27 19:37:34 +0900

    feat: faq 페이지 추가 (#16)

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,7 @@
+export default function FaqPage() {
+  return (
+    <section className='flex min-h-screen w-full flex-col items-center gap-22.25 bg-neutral-100'>
+      <p>FaqPage</p>
+    </section>
+  );
+}
