ea32e9f382ed43c95b68bca8cc32c1fbf43cd988
kimminna <kmina121777@naver.com>
2025-12-27 21:59:42 +0900

    fix(build): 파트 탭 상위 서스펜스 래퍼 적용 (#13)

--- src/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationForm.tsx
+++ src/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationForm.tsx
@@ -3,6 +3,7 @@
 import {AdminApplicationFormTabs} from '@/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationFormTabs';
 import {ApplicationFormEdit} from '@/app/admin/(with-sidebar)/application-form/_components/form/ApplicationFormEdit';
 import {ApplicationFormView} from '@/app/admin/(with-sidebar)/application-form/_components/form/ApplicationFormView';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 import {useAdminApplicationFormStore} from '@/stores/useAdminApplicationFormStore';
 
 export const AdminApplicationForm = () => {
@@ -12,7 +13,9 @@ export const AdminApplicationForm = () => {
 
   return (
     <div className='flex flex-col gap-11.75 rounded-[10px] border border-neutral-300 px-6 py-5'>
-      <AdminApplicationFormTabs />
+      <SuspenseWrapper>
+        <AdminApplicationFormTabs />
+      </SuspenseWrapper>
       <div className='flex flex-col gap-7.5'>
         {isEditingApplicationForm ? (
           <ApplicationFormEdit />
