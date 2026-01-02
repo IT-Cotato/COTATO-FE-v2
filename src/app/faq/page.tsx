71b15fffc8bae404932b350774911a71813ce69c
ion <delion.dev@gmail.com>
2026-01-02 14:45:59 +0900

    feat: faq 페이지 레이아웃 구성 및 컴포넌트 추가 (#16)

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,19 @@
+import HeroMainBanner from '@/components/banner/HeroMainBanner';
+import FaqSideBar from '@/app/faq/_components/FaqSideBar';
+import FaqAccordionList from '@/app/faq/_components/FaqAccordionList';
+import FaqContact from './_components/FaqContact';
+
 export default function FaqPage() {
   return (
     <section className='flex min-h-screen w-full flex-col items-center gap-22.25 bg-neutral-100'>
-      <p>FaqPage</p>
+      <HeroMainBanner />
+      <div className='flex flex-col items-center gap-9.5'>
+        <div className='flex gap-28.25 self-stretch'>
+          <FaqSideBar />
+          <FaqAccordionList />
+        </div>
+        <FaqContact />
+      </div>
     </section>
   );
 }
