dc9e8976de68d97218742c07f5ab84d04d607586
ion <delion.dev@gmail.com>
2026-01-02 15:23:42 +0900

    feat: hero main banner에 heading 및 subheading 추가 (#16)

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,7 +6,10 @@ import FaqContact from '@/app/faq/_components/FaqContact';
 export default function FaqPage() {
   return (
     <section className='flex min-h-screen w-full flex-col items-center gap-22.25 bg-neutral-100'>
-      <HeroMainBanner />
+      <HeroMainBanner
+        heading='COTATO와 함께할 여정이 궁금하신가요?'
+        subheading='자주 묻는 질문에서 답을 찾아 보세요.'
+      />
       <div className='flex flex-col items-center gap-9.5'>
         <div className='flex gap-28.25 self-stretch'>
           <FaqSideBar />
