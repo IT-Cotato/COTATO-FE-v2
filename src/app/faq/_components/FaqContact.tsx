7de5bbc1525923ee96b4720fe5b708d2effbd170
ion <delion.dev@gmail.com>
2026-01-02 14:44:47 +0900

    feat: faq contact 컴포넌트 UI 구현 및 카카오톡 문의 링크 연결 (#16)

---
+++ src/app/faq/_components/FaqContact.tsx
@@ -0,0 +1,18 @@
+const FaqContact = () => {
+  return (
+    <div className='flex flex-col gap-3.5'>
+      <h1 className='self-stretch text-body-l font-normal text-neutral-600'>
+        더 자세한 궁금한 내용이 있다면?
+      </h1>
+      <a
+        href='https://open.kakao.com/o/gyQHDG7e'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='flex items-center justify-center gap-2.5 self-stretch rounded-[20px] bg-primary p-2.5'>
+        <p className='text-body-l text-neutral-800'>카카오톡 채널 문의하기</p>
+      </a>
+    </div>
+  );
+};
+
+export default FaqContact;
