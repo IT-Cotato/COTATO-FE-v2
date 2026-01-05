ccce1d4d624ef4f11c7f0b82e0acea7971fe1bcf
ion <delion.dev@gmail.com>
2026-01-05 16:26:50 +0900

    feat: 구글 로그인 버튼 컴포넌트 추가 (#30)

---
+++ src/components/button/GoogleLoginButton.tsx
@@ -0,0 +1,12 @@
+import GoogleLogo from '@/assets/social/google/google-logo.svg';
+
+export const GoogleLoginButton = () => {
+  return (
+    <button className='flex w-86.25 shrink-0 items-center justify-center gap-3.75 rounded-[10px] bg-white py-3.75 shadow-[0_0_3px_0_rgba(0,0,0,0.08),0_2px_3px_0_rgba(0,0,0,0.17)]'>
+      <GoogleLogo width={24} height={24} />
+      <p className='font-(family-name:--font-roboto) text-xl leading-normal font-medium text-black/54'>
+        Google 계정으로 로그인
+      </p>
+    </button>
+  );
+};
