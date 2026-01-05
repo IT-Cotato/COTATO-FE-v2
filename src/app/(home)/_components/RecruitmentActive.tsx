e425c4a28af2aa35d13d35b950b54defb653dd2c
ion <delion.dev@gmail.com>
2026-01-05 16:34:00 +0900

    feat: 지원하기 버튼 클릭 시 로그인 모달 추가 및 라우팅 처리 (#30)

--- src/app/(home)/_components/RecruitmentActive.tsx
+++ src/app/(home)/_components/RecruitmentActive.tsx
@@ -2,43 +2,67 @@
 
 import Image from 'next/image';
 import {Button} from '@/components/button/Button';
-import Link from 'next/link';
 import {RECRUITMENT_NOTICES} from '@/constants/home/recruitment';
 import {useRecruitmentStore} from '@/store/useRecruitmentStore';
+import {useRouter} from 'next/navigation';
+import {ROUTES} from '@/constants/routes';
+import {useState} from 'react';
+import {LoginModal} from '@/components/modal/LoginModal';
+
+// mockAuthenticated
+const session = false;
 
 export const RecruitmentActive = () => {
+  const router = useRouter();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const generation = useRecruitmentStore((state) => state.generation);
 
-  return (
-    <section className='relative flex min-h-[calc(100dvh-88px)] w-full flex-col items-center justify-center overflow-hidden bg-black px-4 py-10'>
-      <Image
-        src='/background/background.svg'
-        alt='배경 이미지'
-        fill
-        priority
-        className='object-cover object-center'
-      />
-      <div className='relative z-10 flex w-full max-w-[960px] flex-col gap-5'>
-        <h1 className='text-h4 text-white'>
-          🥔 코테이토 {generation}기 지원서 🥔
-        </h1>
-        <div className='flex flex-col gap-8 rounded-[10px] bg-white px-[82px] pt-[44px] pb-[84px]'>
-          <h4 className='text-h4 text-black'>⚠️ 지원 전 유의 사항 ⚠️</h4>
+  const handleApplyClick = () => {
+    if (!session) {
+      setIsModalOpen(true);
+    } else {
+      router.push(ROUTES.APPLY);
+    }
+  };
 
-          <ul className='list-disc space-y-3 pl-6 text-neutral-800'>
-            {RECRUITMENT_NOTICES.map((notice, index) => (
-              <li key={index} className='text-body-l leading-relaxed'>
-                {notice}
-              </li>
-            ))}
-          </ul>
+  return (
+    <>
+      <section className='relative flex min-h-[calc(100dvh-88px)] w-full flex-col items-center justify-center overflow-hidden bg-black px-4'>
+        <Image
+          src='/background/background.svg'
+          alt='배경 이미지'
+          fill
+          priority
+          className='object-cover object-center'
+        />
+        <div className='relative z-10 flex w-full max-w-[960px] flex-col gap-5'>
+          <h1 className='text-h4 text-white'>
+            🥔 코테이토 {generation}기 지원서 🥔
+          </h1>
+          <div className='flex flex-col gap-8 rounded-[10px] bg-white px-[82px] pt-[44px] pb-[84px]'>
+            <h4 className='text-h4 text-black'>⚠️ 지원 전 유의 사항 ⚠️</h4>
+
+            <ul className='list-disc space-y-3 pl-6 text-neutral-800'>
+              {RECRUITMENT_NOTICES.map((notice, index) => (
+                <li key={index} className='text-body-l leading-relaxed'>
+                  {notice}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='flex justify-end'>
+            <Button label='지원하기' onClick={handleApplyClick} />
+          </div>
         </div>
-        <div className='flex justify-end'>
-          <Link href='/apply'>
-            <Button label='지원하기' />
-          </Link>
-        </div>
-      </div>
-    </section>
+      </section>
+
+      <LoginModal
+        title='로그인 후 이용할 수 있는 서비스입니다.'
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 };
