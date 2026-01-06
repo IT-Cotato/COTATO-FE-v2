e29405a506e3b78f5fb5389659aca9fe1be39a79
ion <delion.dev@gmail.com>
2026-01-06 17:09:00 +0900

    feat: oauthLogin api 호출 성공시 사용자 정보 가져오기 추가 (#30)

--- src/hooks/mutations/useAuth.ts
+++ src/hooks/mutations/useAuth.ts
@@ -2,19 +2,38 @@
 
 import {useMutation} from '@tanstack/react-query';
 import {OAuthLoginRequest} from '@/services/types/auth.types';
+import {useRouter} from 'next/navigation';
 import {AxiosError} from 'axios';
-import {oauthLogin} from '@/services/api/auth.api';
-import {setAccessToken, setRefreshToken} from '@/services/utils/tokenManager';
+import {useAuthStore} from '@/store/useAuthStore';
+import {getMe, oauthLogin} from '@/services/api/auth.api';
+import {
+  clearTokens,
+  setAccessToken,
+  setRefreshToken,
+} from '@/services/utils/tokenManager';
 
 /**
  * OAuth 로그인 Mutation Hook
  */
 export const useOAuthLogin = () => {
+  const router = useRouter();
+  const setUser = useAuthStore((state) => state.setUser);
+
   return useMutation({
     mutationFn: (request: OAuthLoginRequest) => oauthLogin(request),
     onSuccess: async (data) => {
       setAccessToken(data.accessToken);
       setRefreshToken(data.refreshToken);
+
+      try {
+        const userResponse = await getMe();
+        setUser(userResponse);
+
+        router.push('/');
+      } catch (error) {
+        console.error('[Failed to fetch user info]:', error);
+        clearTokens();
+      }
     },
     onError: (error: AxiosError) => {
       console.error('[Login failed]', error);
