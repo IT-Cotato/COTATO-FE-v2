f975b40a4912397af20912cd227a8873824b4fba
ion <delion.dev@gmail.com>
2026-01-06 15:17:01 +0900

    feat: auth 관련 api 엔드포인트 추가 (#30)

---
+++ src/services/constant/endpoint.ts
@@ -0,0 +1,10 @@
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const ENDPOINT = {
+  AUTH: {
+    LOGIN_GOOGLE: '/api/auth/login/google',
+    REFRESH: '/api/auth/refresh',
+    LOGOUT: '/api/auth/logout',
+    ME: '/api/auth/me',
+  },
+};
