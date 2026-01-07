d524aef90223ed8ce3859181bf637552f23239c2
ion <delion.dev@gmail.com>
2026-01-07 12:20:50 +0900

    feat: 지원서 작성 페이지 컴포넌트 추가 (#30)

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,14 @@
+/**
+ * 지원서 작성 페이지
+ *
+ * 접근 제한:
+ * - 로그인한 사용자만 접근 가능
+ * - 미로그인 사용자는 alert 후 메인 페이지로 리다이렉트
+ */
+export default function ApplyPage() {
+  return (
+    <section className='flex min-h-screen w-full flex-col items-center bg-neutral-100'>
+      <p>ApplyPage</p>
+    </section>
+  );
+}
