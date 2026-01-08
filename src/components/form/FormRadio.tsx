23f3bc2dabf1660ad66fff34c9c947f835a4d96d
kimminna <kmina121777@naver.com>
2026-01-08 22:22:16 +0900

    refactor(style): 어드민 대응 read-only 스타일 속성 추가 (#34)

--- src/components/form/FormRadio.tsx
+++ src/components/form/FormRadio.tsx
@@ -11,7 +11,10 @@ export const FormRadio = forwardRef<HTMLInputElement, FormRadioProps>(
   function FormRadio({label, className, ...props}, ref) {
     return (
       <label
-        className={clsx('flex cursor-pointer items-center gap-6', className)}>
+        className={clsx(
+          'flex cursor-pointer items-center gap-6 whitespace-nowrap',
+          className
+        )}>
         {/* 라디오 버튼과 안쪽 동그라미를 감싸는 컨테이너 */}
         <div className='relative flex items-center justify-center'>
           <input
@@ -27,7 +30,7 @@ export const FormRadio = forwardRef<HTMLInputElement, FormRadioProps>(
             {...props}
           />
           {/* 3. 선택 시 중앙에 생기는 동그라미 (직접 구현) */}
-          <div className='pointer-events-none absolute size-4.75 scale-0 rounded-full bg-primary transition-transform duration-200 peer-checked:scale-100' />
+          <div className='pointer-events-none absolute size-4.75 scale-0 rounded-full bg-secondary transition-transform duration-200 peer-checked:scale-100' />
         </div>
 
         <span className='text-h5 text-neutral-600'>{label}</span>
