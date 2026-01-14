056eb18f2ded7523e3cefedab6f79571bd2792e2
kimminna <kmina121777@naver.com>
2026-01-15 02:19:16 +0900

    feat: 어드민 지원서 열람 페이지에 모집 공고 API 연동 및 바뀐 컴포넌트 형태에 따른 렌더링 (#47)

--- src/app/admin/(with-sidebar)/application-form/page.tsx
+++ src/app/admin/(with-sidebar)/application-form/page.tsx
@@ -1,15 +1,15 @@
-import {AdminApplicationForm} from '@/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationForm';
-import {AdminApplicationFormControl} from '@/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationFormControl';
-import {AdminApplicationFormHeader} from '@/app/admin/(with-sidebar)/application-form/_components/form/AdminApplicationFormHeader';
-import {AdminRecruitmentInformation} from '@/app/admin/(with-sidebar)/application-form/_components/recruitment/AdminRecruitmentInformation';
+import {AdminApplicationFormContainer} from '@/app/admin/(with-sidebar)/application-form/_containers/AdminApplicationFormContainer';
+import {AdminRecruitmentInformationContainer} from '@/app/admin/(with-sidebar)/application-form/_containers/AdminRecruitmentInformationContainer';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 
 export default function AdminApplicationFormPage() {
   return (
     <section className='flex flex-col gap-6'>
-      <AdminApplicationFormHeader />
-      <AdminRecruitmentInformation />
-      <AdminApplicationFormControl />
-      <AdminApplicationForm />
+      <h1 className='text-h4'>모집 기간</h1>
+      <SuspenseWrapper>
+        <AdminRecruitmentInformationContainer />
+        <AdminApplicationFormContainer />
+      </SuspenseWrapper>
     </section>
   );
 }
