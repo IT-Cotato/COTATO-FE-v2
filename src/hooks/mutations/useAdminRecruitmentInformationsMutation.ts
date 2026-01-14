dd0a7457bd52d33e81bb87919c2678472aa401a5
kimminna <kmina121777@naver.com>
2026-01-15 02:07:59 +0900

    feat: 어드민 모집 공고 관련 뮤테이션, 쿼리 훅 파일 추가 (#47)

---
+++ src/hooks/mutations/useAdminRecruitmentInformationsMutation.ts
@@ -0,0 +1,22 @@
+import {QUERY_KEYS} from '@/constants/query-keys';
+import {PostAdminRecruitmentInformationRequest} from '@/schemas/admin/admin-recruitment-infomation-schema';
+import {postRecruitmentInformations} from '@/services/api/admin/admin.recruitment.info.api';
+import {ErrorResponse} from '@/schemas/common/common-schema';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+
+export const usePostRecruitmentInformationsMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    void,
+    ErrorResponse,
+    PostAdminRecruitmentInformationRequest
+  >({
+    mutationFn: postRecruitmentInformations,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.ADMIN_RECRUITMENT_INFORMATIONS],
+      });
+    },
+  });
+};
---
+++ src/hooks/queries/useAdminRecruitmentInformationsQuery.ts
@@ -0,0 +1,11 @@
+import {QUERY_KEYS} from '@/constants/query-keys';
+import {RecruitmentInformation} from '@/schemas/admin/admin-recruitment-infomation-schema';
+import {getRecruitmentInformations} from '@/services/api/admin/admin.recruitment.info.api';
+import {ErrorResponse} from '@/schemas/common/common-schema';
+import {useQuery} from '@tanstack/react-query';
+
+export const useRecruitmentInformationsQuery = (generationId: number) =>
+  useQuery<RecruitmentInformation, ErrorResponse>({
+    queryKey: [QUERY_KEYS.ADMIN_RECRUITMENT_INFORMATIONS, generationId],
+    queryFn: () => getRecruitmentInformations(generationId),
+  });
