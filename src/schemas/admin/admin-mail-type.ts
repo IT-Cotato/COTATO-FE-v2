313f22115f61e6353e6654cd83cad2028af055b9
choyeon <cho1428hee@naver.com>
2026-01-15 14:12:17 +0900

    refactor: 메일 타입 이름 수정 (#21)

--- src/schemas/admin/admin-mail-type.ts
+++ src/schemas/admin/admin-mail-type.ts
@@ -1,7 +1,7 @@
 export const MAIL_TYPE_MAP = {
   '합격자 메일': 'PASS',
   '불합격자 메일': 'FAIL',
-  '예비 합격자 메일': 'PRELIMINARY',
+  '예비합격자 메일': 'PRELIMINARY',
 } as const;
 
-export const mailTabs = ['합격자 메일', '불합격자 메일', '예비 합격자 메일'];
+export const mailTabs = ['합격자 메일', '불합격자 메일', '예비합격자 메일'];
