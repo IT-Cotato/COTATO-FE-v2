4fef50120d6152066429b3ac595009a75c6b8e90
choyeon <cho1428hee@naver.com>
2026-01-15 10:38:49 +0900

    refactor: 체크박스 조건부 스타일 clsx 이용해서 수정 (#21)

--- src/components/checkbox/CheckBox.tsx
+++ src/components/checkbox/CheckBox.tsx
@@ -1,5 +1,6 @@
 import CheckTrueIcon from '@/assets/check/check-true.svg';
 import CheckFalseIcon from '@/assets/check/check-false.svg';
+import clsx from 'clsx';
 
 interface CheckboxProps {
   checked: boolean;
@@ -11,13 +12,15 @@ export const Checkbox = ({checked, onChange, disabled}: CheckboxProps) => {
   return (
     <div
       onClick={() => !disabled && onChange(!checked)}
-      className={`flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center border transition-colors select-none ${
-        disabled ? 'cursor-not-allowed opacity-50' : ''
-      } ${
-        checked
-          ? 'border-primary bg-primary' // true일 때
-          : 'border-neutral-600 bg-neutral-600' // false일 때
-      }`}>
+      className={clsx(
+        'flex h-4 w-4 shrink-0 items-center justify-center border transition-colors select-none',
+        {
+          'cursor-pointer': !disabled,
+          'cursor-not-allowed opacity-50': disabled,
+          'border-primary bg-primary': checked,
+          'border-neutral-600 bg-neutral-600': !checked,
+        }
+      )}>
       {checked ? <CheckTrueIcon /> : <CheckFalseIcon />}
     </div>
   );
