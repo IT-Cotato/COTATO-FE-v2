b282290a774c7afc894148cf11b3466091af8aa1
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-01-20 10:34:49 +0900

    chore: 키 추가 (#53)

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,6 +1,10 @@
 export const QUERY_KEYS = {
   APPLY: {
     BASIC_INFO: (applicationId: string) => ['apply-basic-info', applicationId],
+    PART_QUESTIONS: (applicationId: string) => [
+      'apply-part-questions',
+      applicationId,
+    ],
   },
   ADMIN_APPLICATION: 'admin-applications',
   ADMIN_RECRUITMENT_INFORMATIONS: 'admin-recruitment-informations',
