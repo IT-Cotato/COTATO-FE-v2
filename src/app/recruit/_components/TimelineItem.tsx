de86a0167c8a800754bc55202574ea67d11f5007
ion <delion.dev@gmail.com>
2026-01-21 11:59:35 +0900

    refactor: 타임라인 아이템 컴포넌트의 props 수정 및 데이터 구조 변경 (#26)

--- src/app/recruit/_components/TimelineItem.tsx
+++ src/app/recruit/_components/TimelineItem.tsx
@@ -1,23 +1,23 @@
 import BrandLogo from '@/assets/brand-logo/brand-logo.svg';
 import ConnectionLine from '@/assets/line/connection-line.svg';
+import {TimelineType} from '@/schemas/recruit/recruit-timeline.schema';
 
 interface TimelineItemProps {
-  phase: string;
-  period: string;
+  item: TimelineType;
 }
 
-export const TimelineItem = ({phase, period}: TimelineItemProps) => {
+export const TimelineItem = ({item}: TimelineItemProps) => {
   return (
     <div className='flex'>
       <div className='flex w-41.5 flex-col items-center gap-3.5'>
         <div className='flex flex-col items-center gap-2.5'>
           <BrandLogo className='h-7 w-7 fill-hover' />
           <p className='text-center text-body-l-sb wrap-anywhere text-neutral-700'>
-            {phase}
+            {item.title}
           </p>
         </div>
         <p className='text-center text-body-l wrap-anywhere whitespace-pre-line text-neutral-600'>
-          {period}
+          {item.date}
         </p>
       </div>
       <ConnectionLine className='mt-4.5 w-11.25 fill-primary' />
