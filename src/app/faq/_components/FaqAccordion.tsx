e073c130d63dba71efb8dd5974bd1ba7027f32ad
ion <delion.dev@gmail.com>
2026-01-22 23:10:46 +0900

    refactor(style): faq 아코디언 컴포넌트 디자인 수정 (#67)

--- src/app/faq/_components/FaqAccordion.tsx
+++ src/app/faq/_components/FaqAccordion.tsx
@@ -17,26 +17,26 @@ export const FaqAccordion = ({question, answer}: FaqAccordionProps) => {
     <button
       onClick={() => setIsOpen(!isOpen)}
       className={clsx(
-        'flex w-257 cursor-pointer flex-col gap-5 self-stretch rounded-[10px] px-5.5 py-2.5 shadow-[0_6px_15px_0_rgba(0,0,0,0.10)] transition-all duration-300',
-        isOpen ? 'bg-neutral-50/85' : 'bg-neutral-50/40'
+        'flex w-full cursor-pointer flex-col gap-5 self-stretch rounded-[10px] px-5.5 py-2.5 shadow-[0_6px_15px_0_rgba(0,0,0,0.10)] transition-all duration-300',
+        isOpen ? 'h-min bg-white' : 'h-min bg-neutral-200'
       )}>
       <div className='flex w-full items-center justify-between self-stretch'>
         <p
           className={clsx(
-            'text-body-l',
+            'text-body-l-sb',
             isOpen ? 'text-neutral-800' : 'text-neutral-600'
           )}>
           {question}
         </p>
         {isOpen ? (
-          <MinusIcon className='h-6 w-6' />
+          <MinusIcon className='h-6 w-6 fill-neutral-600' />
         ) : (
-          <PlusIcon className='h-6 w-6' />
+          <PlusIcon className='h-6 w-6 fill-neutral-600' />
         )}
       </div>
 
       {isOpen && (
-        <p className='self-stretch text-body-m font-normal text-neutral-600'>
+        <p className='self-stretch text-start text-body-m font-normal text-neutral-600'>
           {answer}
         </p>
       )}
