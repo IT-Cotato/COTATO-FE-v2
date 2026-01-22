e393689477b0c6e778c487d7afe1bb29886002a1
ion <delion.dev@gmail.com>
2026-01-22 12:35:46 +0900

    feat(layout): recruitment notice 페이지 레이아웃 개선 및 스타일 일관성 유지 (#26)

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -11,35 +11,37 @@ import {
 
 export default function RecruitmentNoticePage() {
   return (
-    <section>
-      {/* UI 확인용 */}
+    <section className='flex min-w-min justify-center'>
+      <div className='flex w-360 flex-col items-center'>
+        {/* UI 확인용 */}
 
-      <div className='flex w-360 justify-center'>
-        {MOCK_TIMELINE_ITEM.map((item, index) => (
-          <TimelineItem
-            key={item.date}
-            item={item}
-            isLast={index === MOCK_TIMELINE_ITEM.length - 1}
-          />
-        ))}
-      </div>
+        <div className='flex'>
+          {MOCK_TIMELINE_ITEM.map((item, index) => (
+            <TimelineItem
+              key={item.date}
+              item={item}
+              isLast={index === MOCK_TIMELINE_ITEM.length - 1}
+            />
+          ))}
+        </div>
 
-      <div className='flex w-360 justify-center gap-20'>
-        {QUALIFICATIONS_CARD_ITEMS.map((item) => (
-          <QualificationsCard key={item.qualification} item={item} />
-        ))}
-      </div>
+        <div className='flex gap-20'>
+          {QUALIFICATIONS_CARD_ITEMS.map((item) => (
+            <QualificationsCard key={item.qualification} item={item} />
+          ))}
+        </div>
 
-      <div className='flex w-360 justify-center gap-4.5'>
-        {MOCK_RECRUITMENT_POSITION.map((item) => (
-          <RecruitmentPosition key={item.short} item={item} />
-        ))}
-      </div>
+        <div className='flex gap-4.5'>
+          {MOCK_RECRUITMENT_POSITION.map((item) => (
+            <RecruitmentPosition key={item.short} item={item} />
+          ))}
+        </div>
 
-      <div className='flex w-360 flex-wrap justify-center gap-x-5 gap-y-6'>
-        {MOCK_ACTIVITY_CARD.map((item) => (
-          <ActivityCard key={item.short} item={item} />
-        ))}
+        <div className='flex flex-wrap justify-center gap-x-5 gap-y-6'>
+          {MOCK_ACTIVITY_CARD.map((item) => (
+            <ActivityCard key={item.short} item={item} />
+          ))}
+        </div>
       </div>
     </section>
   );
