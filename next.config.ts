37a0b176d252d3151301eacac55d51166ecafe83
ion <delion.dev@gmail.com>
2026-01-22 13:32:59 +0900

    feat(images): 이미지 최적화 및 로딩을 위해 remotePatterns를 추가 (#26)

--- next.config.ts
+++ next.config.ts
@@ -42,6 +42,16 @@ const nextConfig: NextConfig = {
     fileLoaderRule.exclude = /\.svg$/i;
     return config;
   },
+
+  /** images */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
