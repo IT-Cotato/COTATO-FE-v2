725ef9fa6c83164b0601dfda5a32cb0646d5d2e1
ion <delion.dev@gmail.com>
2026-01-24 05:44:46 +0900

    chore: 헤더의 로고 링크를 외부 사이트로 변경 (#88)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -64,7 +64,7 @@ export const Header = () => {
         style={{height: `${HEADER_HEIGHT}px`}}
         className='sticky top-0 z-header flex w-full min-w-360 items-center justify-between bg-black pr-26.25 pl-6.25'>
         <div>
-          <Link href='/'>
+          <Link href='https://www.cotato.kr/' target='_blank'>
             <MainLogo className='w-36.5' />
           </Link>
         </div>
