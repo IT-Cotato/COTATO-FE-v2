0aa2200e12803571b4fd24b006f19522f5e728b5
ion <delion.dev@gmail.com>
2026-01-23 20:21:51 +0900

    feat(style): 배경색을 흰색으로 수정 (#76)

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -2,7 +2,7 @@ import {ContentArea} from '@/app/recruit/_components/ContentArea';
 
 export default function RecruitmentNoticePage() {
   return (
-    <section className='flex min-w-min justify-center'>
+    <section className='flex min-w-min justify-center bg-white'>
       <div className='flex w-360 flex-col items-center'>
         <ContentArea />
       </div>
