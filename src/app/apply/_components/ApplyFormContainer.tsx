cacdab6d2b6a3d3961ef03f04ffaf848c5370532
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-01-23 19:53:37 +0900

    feat: 로딩 상태 처리 추가 (#75)

--- src/app/apply/_components/ApplyFormContainer.tsx
+++ src/app/apply/_components/ApplyFormContainer.tsx
@@ -11,6 +11,7 @@ import HeroMainBanner from '@/components/banner/HeroMainBanner';
 import {AdminRecruitmentInformation} from '@/app/admin/application-edit/_components/recruitment/AdminRecruitmentInformation';
 import {useRecruitmentStatusQuery} from '@/hooks/queries/useRecruitmentStatus.query';
 import {useRecruitmentScheduleQuery} from '@/hooks/queries/useRecruitmentSchedule.query';
+import {Spinner} from '@/components/ui/Spinner';
 
 const STEP_TITLES = {
   1: '기본 인적사항',
@@ -31,10 +32,14 @@ export const ApplyFormContainer = () => {
     handleConfirmSubmit,
   } = useApplyFormController();
 
-  const {data: recruitmentStatus} = useRecruitmentStatusQuery();
+  const {data: recruitmentStatus, isLoading} = useRecruitmentStatusQuery();
   const generation = recruitmentStatus?.data?.generationId;
 
   const {data: scheduleData} = useRecruitmentScheduleQuery();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
