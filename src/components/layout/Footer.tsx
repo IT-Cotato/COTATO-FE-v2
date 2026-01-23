f4e3f0c2f8481ad7839cbbd10323684150bfa915
ion <delion.dev@gmail.com>
2026-01-24 05:16:16 +0900

    fix(style): footer 컴포넌트 스타일 일관성 수정 (#88)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer
       style={{height: `${FOOTER_HEIGHT}px`}}
-      className='flex min-w-360 flex-none items-center justify-center bg-black px-[62px] py-[47px] text-white'>
+      className='flex min-w-360 items-center justify-between bg-black px-15.5 py-12.75'>
       <div className='flex w-full items-end justify-between'>
         <FooterInfo />
         <FooterContact />
