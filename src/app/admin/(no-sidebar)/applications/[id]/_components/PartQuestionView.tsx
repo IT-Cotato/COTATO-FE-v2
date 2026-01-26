352ac4c8e2a5e9f950eed449e88325f9128144df
kimminna <kmina121777@naver.com>
2026-01-26 20:49:04 +0900

    refactor: 포트폴리오 질문 여부 확인용 로직을 데이터 마지막 인덱스 여부로 변경 (#96)

--- src/app/admin/(no-sidebar)/applications/[id]/_components/PartQuestionView.tsx
+++ src/app/admin/(no-sidebar)/applications/[id]/_components/PartQuestionView.tsx
@@ -16,32 +16,42 @@ export const PartQuestionView = ({
   questionsWithAnswers,
   pdfFileUrl,
 }: PartQuestionViewProps) => {
+  /** 포트폴리오 질문 확인용 변수 */
+  const lastSequence =
+    questionsWithAnswers[questionsWithAnswers.length - 1]?.sequence;
+
   return (
     <div className='flex flex-col gap-10'>
-      {questionsWithAnswers.map((data) => (
-        <div key={data.sequence} className='flex flex-col gap-5'>
-          {/* 6번 질문  (포트폴리오) */}
-          {data.sequence === 6 ? (
-            pdfFileUrl ? (
-              <FormLink
+      {questionsWithAnswers.map((data) => {
+        const isLastQuestion = data.sequence === lastSequence;
+
+        return (
+          <div key={data.sequence} className='flex flex-col gap-5'>
+            {isLastQuestion ? (
+              pdfFileUrl ? (
+                <FormLink
+                  label={`${data.sequence}. ${data.questionContent}`}
+                  readOnly
+                  value={pdfFileUrl}
+                />
+              ) : (
+                <p className='flex justify-center text-h5 text-neutral-400'>
+                  첨부된 포트폴리오가 없습니다.
+                </p>
+              )
+            ) : (
+              <FormTextarea
                 label={`${data.sequence}. ${data.questionContent}`}
                 readOnly
-                value={pdfFileUrl}
+                value={data.content ?? ''}
+                maxLength={500}
+                currentLength={data.byteSize}
               />
-            ) : (
-              <p className='text-neutral-400'>첨부된 포트폴리오가 없습니다.</p>
-            )
-          ) : (
-            <FormTextarea
-              label={`${data.sequence}.${data.questionContent}`}
-              readOnly
-              value={data.content ?? ''}
-              maxLength={500}
-              currentLength={data.byteSize}
-            />
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        );
+      })}
+
       <div className='flex flex-row gap-7.5'>
         <FullButton
           label='이전'
