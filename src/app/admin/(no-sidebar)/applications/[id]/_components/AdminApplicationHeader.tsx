89a03fe11f489151b66ba9dd5cd7be63d9f94846
kimminna <kmina121777@naver.com>
2026-01-26 22:23:54 +0900

    chore(review): 불필요한 클라이언트 선언부 제거 (#96)

--- src/app/admin/(no-sidebar)/applications/[id]/_components/AdminApplicationHeader.tsx
+++ src/app/admin/(no-sidebar)/applications/[id]/_components/AdminApplicationHeader.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {AdminApplicationBasicInfoType} from '@/schemas/admin/admin-application.schema';
 import LogoIcon from '@/assets/small-logo/small-logo.svg';
 import {PART_TABS} from '@/constants/common/part';
