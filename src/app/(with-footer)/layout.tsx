068ba2724a854a1930f2dfa46e6e0cdc898fc905
kimminna <kmina121777@naver.com>
2026-01-26 20:20:05 +0900

    fix(file): 푸터를 포함하는 레이아웃 함수명 변경 (#96)

--- src/app/(with-footer)/layout.tsx
+++ src/app/(with-footer)/layout.tsx
@@ -2,7 +2,7 @@ import Providers from '@/app/providers';
 import {Footer} from '@/components/layout/Footer';
 import {ConditionalAuthProvider} from '@/components/providers/ConditionalAuthProvider';
 
-export default function AdminNoSideBarLayout({
+export default function WithFooterLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
