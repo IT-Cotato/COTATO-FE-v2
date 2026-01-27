9242df428f6d7e98a65b7c00ce26e349c6d3498b
kimminna <kmina121777@naver.com>
2026-01-27 15:02:07 +0900

    fix(build): 기존 모집 공고 스키마 타입명 변경 (#96)

--- src/hooks/mutations/useAdminRecruitmentInformations.mutation.ts
+++ src/hooks/mutations/useAdminRecruitmentInformations.mutation.ts
@@ -1,6 +1,5 @@
 import {QUERY_KEYS} from '@/constants/query-keys';
 import type {PostAdminRecruitmentInformationRequest} from '@/schemas/admin/admin-recruitment-information.schema';
-
 import type {ErrorResponse} from '@/schemas/common/common-schema';
 import {postAdminRecruitmentInformations} from '@/services/api/admin/admin-recruitment-info.api';
 import {useMutation, useQueryClient} from '@tanstack/react-query';
