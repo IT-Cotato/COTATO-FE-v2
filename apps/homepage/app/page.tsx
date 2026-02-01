07041be9a911777ab9fd54660cdf49a1887634a2
kimminna <kmina121777@naver.com>
2026-02-01 18:33:14 +0900

    chore: 기본 docs, web 구조 삭제 및 svgr-config 패키지 추가 (#1)

---
+++ apps/homepage/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from '@repo/ui/components/buttons/Button';
+
+export default function Home() {
+  return (
+    <>
+      home
+      <Button label='모노레포 packages/ui에서 가져온 버튼 컴포넌트임' />
+    </>
+  );
+}
