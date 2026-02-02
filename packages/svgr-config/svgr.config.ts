356e486b1ba6d5d85bdb30eeacc6a3b2d88ac35f
kimminna <kmina121777@naver.com>
2026-02-02 15:44:16 +0900

    feat: svgr 패키지 모듈화 (#1)

---
+++ packages/svgr-config/svgr.config.ts
@@ -0,0 +1,52 @@
+const svgrConfig = {
+  // @ts-expect-error type error
+  webpack: (config) => {
+    // @ts-expect-error type error
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      resourceQuery: {
+        not: [...fileLoaderRule.resourceQuery.not, /url/],
+      }, // exclude if *.svg?url
+      use: ['@svgr/webpack'],
+    });
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ref: true,
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+        as: '*.tsx',
+      },
+    },
+  },
+};
+
+export default svgrConfig;
