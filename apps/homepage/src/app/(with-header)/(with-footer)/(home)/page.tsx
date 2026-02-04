e7bb2cde4d0f9bae7ab9cbb96991e9b541a6562a
kimminna <kmina121777@naver.com>
2026-02-05 02:52:51 +0900

    feat(homepage): 페이지 레이아웃 구성 (#7)

--- apps/homepage/src/app/(with-header)/(with-footer)/(home)/page.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/(home)/page.tsx
@@ -1,3 +1,21 @@
+import {HomeCoreValue} from '@/app/(with-header)/(with-footer)/(home)/_components/HomeCoreValue';
+import {HomeFinalSection} from '@/app/(with-header)/(with-footer)/(home)/_components/HomeFinalSection';
+import {HomeBanner} from '@/app/(with-header)/(with-footer)/(home)/_components/HomeBanner';
+import {HomeCotatoReviewContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomeCotatoReviewContainer';
+import {HomePartSectionContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomePartSectionContainer';
+import {HomeMainScheduleContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomeMainSheduleContainer';
+
 export default function HomePage() {
-  return <div>HOME</div>;
+  return (
+    <section>
+      <HomeBanner />
+      <div className='flex flex-col items-center gap-50 bg-white pt-25 pb-50'>
+        <HomeCoreValue />
+        <HomeMainScheduleContainer />
+        <HomePartSectionContainer />
+        <HomeCotatoReviewContainer />
+        <HomeFinalSection />
+      </div>
+    </section>
+  );
 }
