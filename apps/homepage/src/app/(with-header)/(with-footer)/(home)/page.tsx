9082b03daa4036a89d8aa24004f0615a7b5a0860
kimminna <kmina121777@naver.com>
2026-02-05 02:58:06 +0900

    feat(homepage): searchParams fallback을 위한 서스펜스 래퍼 컴포넌트 추가 (#7)

--- apps/homepage/src/app/(with-header)/(with-footer)/(home)/page.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/(home)/page.tsx
@@ -4,6 +4,7 @@ import {HomeBanner} from '@/app/(with-header)/(with-footer)/(home)/_components/H
 import {HomeCotatoReviewContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomeCotatoReviewContainer';
 import {HomePartSectionContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomePartSectionContainer';
 import {HomeMainScheduleContainer} from '@/app/(with-header)/(with-footer)/(home)/_containers/HomeMainSheduleContainer';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 
 export default function HomePage() {
   return (
@@ -12,7 +13,9 @@ export default function HomePage() {
       <div className='flex flex-col items-center gap-50 bg-white pt-25 pb-50'>
         <HomeCoreValue />
         <HomeMainScheduleContainer />
-        <HomePartSectionContainer />
+        <SuspenseWrapper>
+          <HomePartSectionContainer />
+        </SuspenseWrapper>
         <HomeCotatoReviewContainer />
         <HomeFinalSection />
       </div>
