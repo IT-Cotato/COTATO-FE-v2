7d4855caefd5060ff635d723a23530ec606b3893
ion <delion.dev@gmail.com>
2026-02-05 02:45:43 +0900

    fix(recruit): faq 섹션의 텍스트 스타일 수정 (#15)

--- apps/recruit/src/app/(with-footer)/faq/_components/FaqAccordion.tsx
+++ apps/recruit/src/app/(with-footer)/faq/_components/FaqAccordion.tsx
@@ -24,7 +24,7 @@ export const FaqAccordion = ({item}: FaqAccordionProps) => {
       <div className='flex w-full items-center justify-between self-stretch'>
         <p
           className={clsx(
-            'text-body-l-sb transition-colors duration-300',
+            'text-h5 transition-colors duration-300',
             isOpen ? 'text-neutral-800' : 'text-neutral-600'
           )}>
           {item.question}
@@ -55,7 +55,7 @@ export const FaqAccordion = ({item}: FaqAccordionProps) => {
             ? 'grid-rows-[1fr] pt-5 opacity-100'
             : 'grid-rows-[0fr] pt-0 opacity-0'
         )}>
-        <p className='overflow-hidden text-start text-body-m font-normal text-neutral-600'>
+        <p className='text-body-l overflow-hidden text-start font-normal text-neutral-600'>
           {item.answer}
         </p>
       </div>
