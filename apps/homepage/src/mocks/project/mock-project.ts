cf206d0a06cd7b49fe2eeb4509f92a21ac06d32b
choyeon <cho1428hee@naver.com>
2026-02-05 13:10:13 +0900

    chore(homepage): 프로젝트 목데이터 생성 (#8)

---
+++ apps/homepage/src/mocks/project/mock-project.ts
@@ -0,0 +1,22 @@
+export const PROJECTS_MOCK_DATA = [
+  {
+    projectId: 1,
+    name: 'Troublog',
+    shortDescription: '개발자의 트러블슈팅이 성장으로 이어지는 곳',
+    projectType: 'DEMODAY',
+    generationId: 11,
+    thumbnailUrl: 'https://picsum.photos/330/200',
+    projectLink: 'https://github.com',
+  },
+  ...Array(8)
+    .fill(null)
+    .map((_, i) => ({
+      projectId: i + 2,
+      name: 'Troublog',
+      shortDescription: '개발자의 트러블슈팅이 성장으로 이어지는 곳',
+      projectType: i % 2 === 0 ? 'DEMODAY' : 'HACKATHON',
+      generationId: 11,
+      thumbnailUrl: 'https://picsum.photos/330/200',
+      projectLink: 'https://github.com',
+    })),
+];
