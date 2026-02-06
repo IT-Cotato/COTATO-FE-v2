ff9b8e4c9c2e629df44e462bcbb1ad8019318c8a
ion <delion.dev@gmail.com>
2026-02-06 10:58:17 +0900

    feat(recruit): 구독 알림 성공 시 쿼리 무효화 추가 (#17)

--- apps/recruit/src/hooks/mutations/useRecruitment.mutation.ts
+++ apps/recruit/src/hooks/mutations/useRecruitment.mutation.ts
@@ -1,8 +1,16 @@
+import {QUERY_KEYS} from '@/constants/query-keys';
 import {postRecruitmentNotify} from '@/services/api/recruitment/recruitment.api';
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 export const useSubscribeRecruitmentNotify = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postRecruitmentNotify,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.MAIL_STATUS],
+      });
+    },
   });
 };
