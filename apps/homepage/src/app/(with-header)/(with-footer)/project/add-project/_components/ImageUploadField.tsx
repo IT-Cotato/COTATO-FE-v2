2b7b30ec85ccd02c5360e2edbca7dcae2661d498
choyeon <cho1428hee@naver.com>
2026-02-08 13:31:30 +0900

    fix(homepage): 함수 컨벤션 핸들러로 수정 (#28)

--- apps/homepage/src/app/(with-header)/(with-footer)/project/add-project/_components/ImageUploadField.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/project/add-project/_components/ImageUploadField.tsx
@@ -34,7 +34,7 @@ export const ImageUploadField = ({
     useSensor(KeyboardSensor, {coordinateGetter: sortableKeyboardCoordinates})
   );
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) handleUpload(e.target.files);
     if (fileInputRef.current) fileInputRef.current.value = '';
   };
@@ -58,7 +58,7 @@ export const ImageUploadField = ({
           className='hidden'
           accept='.jpg,.png,.jpeg'
           multiple
-          onChange={onFileChange}
+          onChange={handleFileChange}
         />
       </div>
       <ImageSortableList
