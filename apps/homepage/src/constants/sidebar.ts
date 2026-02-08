4e3fd88c8016258704fefaa7d0af5bb65329dbb3
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-08 16:50:40 +0900

    feat(homepage): 사이드바 네비 구성 추가 (#33)

--- apps/homepage/src/constants/sidebar.ts
+++ apps/homepage/src/constants/sidebar.ts
@@ -2,23 +2,37 @@ import MyActivityIcon from '@/assets/sidebar/my-activity.svg';
 import AttendanceIcon from '@/assets/sidebar/attendance.svg';
 import CSQuizIcon from '@/assets/sidebar/cs-quiz.svg';
 import ApprovalIcon from '@/assets/sidebar/approval.svg';
-import SessionIcon from '@/assets/sidebar/session.svg';
-import PenaltyIcon from '@/assets/sidebar/penalty.svg';
-import {ROUTES} from '@/constants/routes';
+import SessionManagementIcon from '@/assets/sidebar/session-management.svg';
+import AttendanceManagementIcon from '@/assets/sidebar/attendance-management.svg';
+import PenaltyManagementIcon from '@/assets/sidebar/penalty-management.svg';
 
-export const SIDEBAR_NAV_GROUPS = [
+import {ROUTES} from '@/constants/routes';
+import {FC, SVGProps} from 'react';
+
+interface NavItem {
+  label: string;
+  href: string;
+  icon?: FC<SVGProps<SVGElement>>;
+}
+
+interface NavGroup {
+  title: string;
+  items: NavItem[];
+}
+
+export const SIDEBAR_NAV_GROUPS: NavGroup[] = [
   {
     title: '마이메뉴',
     items: [
-      {label: '나의 활동', href: ROUTES.MYPAGE, icon: MyActivityIcon},
+      {label: '나의 활동', href: ROUTES.MYPAGE_ACTIVITY, icon: MyActivityIcon},
       {
         label: '출석하기',
-        href: `${ROUTES.MYPAGE}/attendance`,
+        href: ROUTES.MYPAGE_ATTENDANCE,
         icon: AttendanceIcon,
       },
       {
-        label: 'CSQUIZ 바로가기',
-        href: `${ROUTES.MYPAGE}/csquiz`,
+        label: 'CS QUIZ 바로가기',
+        href: ROUTES.MYPAGE_CSQUIZ,
         icon: CSQuizIcon,
       },
     ],
@@ -26,19 +40,31 @@ export const SIDEBAR_NAV_GROUPS = [
   {
     title: '설정',
     items: [
-      {label: '보안 설정', href: `${ROUTES.MYPAGE}/security`},
-      {label: '운영 규정 및 서비스 약관', href: `${ROUTES.MYPAGE}/terms`},
+      {label: '보안 설정', href: ROUTES.MYPAGE_SECURITY},
+      {label: '운영 규정 및 서비스 약관', href: ROUTES.MYPAGE_TERMS},
     ],
   },
 ];
 
-export const ADMIN_NAV_GROUP = {
+export const ADMIN_NAV_GROUP: NavGroup = {
   title: '관리자 메뉴',
   items: [
     {label: '회원 관리', href: ROUTES.ADMIN_USERS, icon: MyActivityIcon},
     {label: '가입 승인', href: ROUTES.ADMIN_APPROVALS, icon: ApprovalIcon},
-    {label: '세션 관리', href: ROUTES.ADMIN_SESSIONS, icon: SessionIcon},
-    {label: '출석 관리', href: ROUTES.ADMIN_ATTENDANCE, icon: AttendanceIcon},
-    {label: '상벌점 관리', href: ROUTES.ADMIN_PENALTIES, icon: PenaltyIcon},
+    {
+      label: '세션 관리',
+      href: ROUTES.ADMIN_SESSIONS,
+      icon: SessionManagementIcon,
+    },
+    {
+      label: '출석 관리',
+      href: ROUTES.ADMIN_ATTENDANCE,
+      icon: AttendanceManagementIcon,
+    },
+    {
+      label: '상벌점 관리',
+      href: ROUTES.ADMIN_PENALTIES,
+      icon: PenaltyManagementIcon,
+    },
   ],
 };
