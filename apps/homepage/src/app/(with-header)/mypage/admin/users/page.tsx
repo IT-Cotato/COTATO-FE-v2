3997308d81a2129edcf331a45d29cfa12e2103d3
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-10 22:50:28 +0900

    feat(homepage): 관리자 회원 관리 페이지 구성 (#46)

--- apps/homepage/src/app/(with-header)/mypage/admin/users/page.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/users/page.tsx
@@ -1,3 +1,12 @@
+import {AdminUsersContainer} from './_containers/AdminUsersContainer';
+
 export default function AdminUsersPage() {
-  return <div>회원 관리</div>;
+  return (
+    <section className='flex flex-col p-12.5'>
+      <div className='flex min-w-275 flex-col gap-13.25'>
+        <h1 className='text-h4'>회원 관리</h1>
+        <AdminUsersContainer />
+      </div>
+    </section>
+  );
 }
