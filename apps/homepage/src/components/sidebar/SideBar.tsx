b57285b928a7e6ba9d219fc030fb188960f7714d
choyeon <cho1428hee@naver.com>
2026-02-10 16:12:56 +0900

    ui(homepage): 마이페이지 사이드바 레이아웃, 스타일 수정 및 로그아웃 버튼 추가 (#55)

--- apps/homepage/src/components/sidebar/SideBar.tsx
+++ apps/homepage/src/components/sidebar/SideBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {SIDEBAR_NAV_GROUPS, ADMIN_NAV_GROUP} from '@/constants/sidebar';
+import {FullButton} from '@repo/ui/components/buttons/FullButton';
 import clsx from 'clsx';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
@@ -15,8 +16,13 @@ export const SideBar = ({isAdmin = false}: SideBarProps) => {
     ? [...SIDEBAR_NAV_GROUPS, ADMIN_NAV_GROUP]
     : SIDEBAR_NAV_GROUPS;
 
+  // 로그아웃 핸들러
+  const handleLogout = () => {
+    console.log('로그아웃 처리');
+  };
+
   return (
-    <nav className='z-sidebar sticky top-30 flex max-h-[calc(100vh-7.5rem)] flex-col gap-7.5 overflow-y-auto px-6.25'>
+    <nav className='z-sidebar no-scrollbar sticky top-30 flex max-h-[calc(100vh-7.5rem)] flex-col gap-7.5 overflow-y-auto px-6.25'>
       {groups.map(({title, items}) => (
         <div key={title} className='flex flex-col gap-4.75'>
           <h4 className='text-h4 text-neutral-400'>{title}</h4>
@@ -24,14 +30,13 @@ export const SideBar = ({isAdmin = false}: SideBarProps) => {
             {items.map(({label, href, icon: Icon}) => {
               const isActive =
                 pathname === href || pathname.startsWith(`${href}/`);
-
               return (
                 <li key={href}>
                   <Link
                     href={href}
                     aria-current={isActive ? 'page' : undefined}
                     className={clsx(
-                      'text-h5 flex w-full items-center gap-2 rounded-[10px] px-2 py-2.5 transition-colors',
+                      'text-h5 flex w-full items-center gap-2 rounded-[5px] px-2 py-2.5 transition-colors',
                       isActive
                         ? 'bg-neutral-800 text-neutral-100'
                         : 'text-neutral-800'
@@ -45,6 +50,17 @@ export const SideBar = ({isAdmin = false}: SideBarProps) => {
           </ul>
         </div>
       ))}
+      <FullButton
+        label='로그아웃'
+        variant='outline'
+        height={40}
+        backgroundColor='neutral-50'
+        textColor='primary'
+        labelTypo='body_l_sb'
+        borderRadius={30}
+        onClick={handleLogout}
+        wrapperClassName={clsx('mb-10', isAdmin ? 'mt-[175px]' : 'mt-[505px]')}
+      />
     </nav>
   );
 };
