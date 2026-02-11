82d0dab51d5b1e576c8e6e1c239488cf00dc074e
kimminna <kmina121777@naver.com>
2026-02-11 21:56:35 +0900

    fix(recruit): 마이페이지 상위 서스펜스 래퍼 적용 (#62)

--- apps/recruit/src/app/(with-footer)/my-page/detail/page.tsx
+++ apps/recruit/src/app/(with-footer)/my-page/detail/page.tsx
@@ -1,9 +1,12 @@
 import {MyPageApplicationDetailContainer} from '@/app/(with-footer)/my-page/detail/_containers/MyPageApplicationDetailContainer';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 
 export default function MyPageApplicationDetailPage() {
   return (
     <section className='bg-white'>
-      <MyPageApplicationDetailContainer />
+      <SuspenseWrapper>
+        <MyPageApplicationDetailContainer />
+      </SuspenseWrapper>
     </section>
   );
 }
