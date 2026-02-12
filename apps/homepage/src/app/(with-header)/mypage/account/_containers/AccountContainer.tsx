18afbb233fea8c9582272e778a223ea0003ad700
choyeon <cho1428hee@naver.com>
2026-02-13 01:45:00 +0900

    ui(homepage): 탈퇴하기 UI 구현 (#71)

--- apps/homepage/src/app/(with-header)/mypage/account/_containers/AccountContainer.tsx
+++ apps/homepage/src/app/(with-header)/mypage/account/_containers/AccountContainer.tsx
@@ -1,10 +1,11 @@
+import {DeleteAccountContainer} from '@/app/(with-header)/mypage/account/_containers/DeleteAccountContainer';
 import {ResetPWContainer} from '@/app/(with-header)/mypage/account/_containers/ResetPWContainer';
 
 export const AccountContainer = () => {
   return (
     <div className='flex flex-col gap-[50px]'>
       <ResetPWContainer />
-      회원 탈퇴
+      <DeleteAccountContainer />
     </div>
   );
 };
---
+++ apps/homepage/src/app/(with-header)/mypage/account/_containers/DeleteAccountContainer.tsx
@@ -0,0 +1,23 @@
+import {Button} from '@repo/ui/components/buttons/Button';
+
+export const DeleteAccountContainer = () => {
+  return (
+    <div className='flex items-end justify-between self-stretch'>
+      <div className='flex h-[94px] w-[718px] flex-col gap-1'>
+        <h3 className='text-h3 text-neutral-700'>회원 탈퇴</h3>
+        <span className='text-h5 text-neutral-600'>
+          탈퇴 신청 시 해당 계정은 30일동안 비활성화 상태로 전환됩니다.
+          <br />
+          비활성화 기간 동안 코테이토 홈페이지 사용이 중단됩니다.
+        </span>
+      </div>
+      <Button
+        label='탈퇴하기'
+        labelTypo='body_l_sb'
+        textColor='white'
+        width={134}
+        height={42}
+      />
+    </div>
+  );
+};
