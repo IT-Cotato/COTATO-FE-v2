41ca3a9fb5e60dad98be92e57f6099b6be227c5a
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-12 13:45:50 +0900

    style(homepage): 디자인 수정 사항 반영에 따른 간격 수정 (#46)

--- apps/homepage/src/app/(with-header)/mypage/admin/users/page.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/users/page.tsx
@@ -1,10 +1,10 @@
-import { SuspenseWrapper } from '@/components/wrappers/SuspenseWrapper';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
 import {AdminUsersContainer} from './_containers/AdminUsersContainer';
 
 export default function AdminUsersPage() {
   return (
     <section className='flex flex-col p-12.5'>
-      <div className='flex min-w-275 flex-col gap-13.25'>
+      <div className='flex min-w-275 flex-col gap-6.75'>
         <h1 className='text-h4'>회원 관리</h1>
         <SuspenseWrapper>
           <AdminUsersContainer />
