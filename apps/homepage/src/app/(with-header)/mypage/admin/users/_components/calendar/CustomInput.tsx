8d104f7089893e76fbc3692ae39ae91cc5329ff0
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-13 14:00:01 +0900

    feat(homepage): customInput 확장성을 위한 props 추가 (#48)

--- apps/homepage/src/app/(with-header)/mypage/admin/users/_components/calendar/CustomInput.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/users/_components/calendar/CustomInput.tsx
@@ -8,10 +8,23 @@ interface CustomInputProps {
   disabled?: boolean;
   placeholder?: string;
   hideIcon?: boolean;
+  className?: string;
+  textAlign?: 'left' | 'center';
 }
 
 export const CustomInput = forwardRef<HTMLButtonElement, CustomInputProps>(
-  function CustomInput({value, onClick, disabled, placeholder, hideIcon}, ref) {
+  function CustomInput(
+    {
+      value,
+      onClick,
+      disabled,
+      placeholder,
+      hideIcon,
+      className,
+      textAlign = 'center',
+    },
+    ref
+  ) {
     return (
       <button
         type='button'
@@ -19,14 +32,25 @@ export const CustomInput = forwardRef<HTMLButtonElement, CustomInputProps>(
         onClick={onClick}
         disabled={disabled}
         className={clsx(
-          'text-body-l flex h-9 w-[153px] items-center justify-between gap-2 rounded-[5px] bg-white px-2.5 text-neutral-800',
+          'text-body-l flex h-9 items-center justify-between gap-2 rounded-[5px] px-2.5 text-neutral-800',
+          !className?.includes('w-') && 'w-38.25', // 기본 너비
+          !className?.includes('h-') && 'h-9', // 기본 높이
+          !className?.includes('bg-') && 'bg-white', // 기본 배경색
           {
             'cursor-pointer': !disabled,
             'cursor-default': disabled,
-          }
+          },
+          className
         )}>
-        <span className='flex-1 text-center'>{value || placeholder}</span>
-        {!hideIcon && <CalendarIcon className='h-5 w-5 hover:text-primary' />}
+        <span
+          className={clsx(
+            'flex-1',
+            textAlign === 'center' ? 'text-center' : 'text-left',
+            !value && 'text-neutral-500'
+          )}>
+          {value || placeholder}
+        </span>
+        {!hideIcon && <CalendarIcon className='hover:text-primary h-5 w-5' />}
       </button>
     );
   }
