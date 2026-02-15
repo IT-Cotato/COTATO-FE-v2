48372bada2e028ca6bb02cb9177830696f0d41e1
kimminna <kmina121777@naver.com>
2026-02-16 01:09:50 +0900

    chore(root): 비유저블 스크립트 삽입 (#108)

---
+++ apps/homepage/src/lib/BeUsableRum.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/script';
+
+export default function BeUsableRum() {
+  return (
+    <>
+      <Script
+        id='beusable-rum'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w, d, a){
+                  w.__beusablerumclient__ = {
+                      load : function(src){
+                          var b = d.createElement("script");
+                          b.src = src; b.async=true; b.type = "text/javascript";
+                          d.getElementsByTagName("head")[0].appendChild(b);
+                      }
+                  };w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+              })(window, document, "//rum.beusable.net/load/b260216e005628u880");
+            `,
+        }}
+      />
+    </>
+  );
+}
