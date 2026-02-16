30e3c221332ac9cce1c49c4c0598b4a7187126e4
kimminna <kmina121777@naver.com>
2026-02-16 22:31:01 +0900

    fix(homepage): 비밀번호 특수문자 포함 로직 오류 수정 (#115)

--- apps/homepage/src/schemas/auth/auth.schema.ts
+++ apps/homepage/src/schemas/auth/auth.schema.ts
@@ -32,7 +32,7 @@ export const JoinRequestSchema = z.object({
     .string()
     .min(8, {message: '비밀번호는 최소 8자 이상이어야 합니다.'})
     .max(16, {message: '비밀번호는 최대 16자 이내여야 합니다.'})
-    .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/, {
+    .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,16}$/, {
       message: '비밀번호는 영어, 숫자, 특수문자를 포함하여 8~16자여야 합니다.',
     })
     .or(z.literal('')),
