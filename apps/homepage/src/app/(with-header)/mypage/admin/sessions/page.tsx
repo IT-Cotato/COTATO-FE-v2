9ce6c5f8489472aa32f52c4fc4ece28233b85bbd
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-17 01:12:02 +0900

    feat(homepage): 세션 관리 페이지 구성 반영 (#114)

--- apps/homepage/src/app/(with-header)/mypage/admin/sessions/page.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/sessions/page.tsx
@@ -1,3 +1,15 @@
+import {SessionsContainer} from '@/app/(with-header)/mypage/admin/sessions/_containers/SessionsContainer';
+import {SuspenseWrapper} from '@/components/wrappers/SuspenseWrapper';
+
 export default function AdminSessionsPage() {
-  return <div>세션 관리</div>;
+  return (
+    <section className='flex flex-col p-12.5'>
+      <div className='flex min-w-275 flex-col gap-13'>
+        <h1 className='text-h4'>세션 관리</h1>
+        <SuspenseWrapper>
+          <SessionsContainer />
+        </SuspenseWrapper>
+      </div>
+    </section>
+  );
 }
