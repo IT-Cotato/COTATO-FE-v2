97e23167df5ae4dc3a69d68db63479c598ccb804
choyeon <cho1428hee@naver.com>
2026-02-18 01:44:11 +0900

    feat(homepage): 활동 부원이 아닐 때의 나의 활동 예외 UI 구현 (#140)

--- apps/homepage/src/app/(with-header)/mypage/activity/_containers/AttendanceContainer.tsx
+++ apps/homepage/src/app/(with-header)/mypage/activity/_containers/AttendanceContainer.tsx
@@ -1,19 +1,46 @@
 'use client';
 
+import {useState} from 'react';
+import {AxiosError} from 'axios';
 import {AttendanceTab} from '@/app/(with-header)/mypage/activity/_components/AttendanceTab';
 import {AttendanceCheckContainer} from '@/app/(with-header)/mypage/activity/_containers/AttendanceCheckContainer';
 import {AttendanceStatusContainer} from '@/app/(with-header)/mypage/activity/_containers/AttendanceStatusContainer';
 import {TabType} from '@/schemas/mypage-mem/activity/mypage-mem-type';
-import {useState} from 'react';
+import {
+  useAttendanceDashboardQuery,
+  usePenaltyDashboardQuery,
+} from '@/hooks/queries/useActivity.queries';
+import {ErrorResponse} from '@/schemas/common/common-schema';
+import {NotActiveMemberView} from '@/app/(with-header)/mypage/activity/_components/NotActiveMemberView';
 
 export const AttendanceContainer = () => {
   const [activeTab, setActiveTab] = useState<TabType>('attendance');
+  const isAttendance = activeTab === 'attendance';
+
+  // 하나라도 NP-002면 활동 회원이 아닌 것으로 판단함
+  const {error: attendError} = useAttendanceDashboardQuery({
+    enabled: isAttendance,
+  });
+  const {error: penaltyError} = usePenaltyDashboardQuery({
+    enabled: !isAttendance,
+  });
+
+  const currentError = (
+    isAttendance ? attendError : penaltyError
+  ) as AxiosError<ErrorResponse> | null;
+  const isNotActiveMember = currentError?.response?.data?.code === 'NP-002';
 
   return (
     <div className='flex w-full flex-col items-center gap-7.5'>
-      <AttendanceTab activeTab={activeTab} onTabChange={setActiveTab} />
-      <AttendanceStatusContainer activeTab={activeTab} />
-      <AttendanceCheckContainer activeTab={activeTab} />
+      {!isNotActiveMember ? (
+        <>
+          <AttendanceTab activeTab={activeTab} onTabChange={setActiveTab} />
+          <AttendanceStatusContainer activeTab={activeTab} />
+          <AttendanceCheckContainer activeTab={activeTab} />
+        </>
+      ) : (
+        <NotActiveMemberView />
+      )}
     </div>
   );
 };
