a9c091c280dc7b5d8f6eae8814b1af712dac51cd
choyeon <cho1428hee@naver.com>
2026-02-18 01:45:59 +0900

    ui(homepage): typo 변경 (#140)

--- apps/homepage/src/app/(with-header)/mypage/activity/_components/NotActiveMemberView.tsx
+++ apps/homepage/src/app/(with-header)/mypage/activity/_components/NotActiveMemberView.tsx
@@ -1,5 +1,5 @@
 export const NotActiveMemberView = () => (
-  <div className='text-body-l flex min-h-screen w-full items-center justify-center text-neutral-400'>
+  <div className='text-h5 flex min-h-screen w-full items-center justify-center text-neutral-400'>
     활동 회원이 아닙니다.
   </div>
 );
