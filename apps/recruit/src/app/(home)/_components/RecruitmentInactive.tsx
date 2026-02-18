5c3d4e728d845ee11964e313543e8caa365447fd
ion <delion.dev@gmail.com>
2026-02-19 00:24:54 +0900

    fix(recruit): 모집 비활성 컴포넌트이므로 모집 상태 조회 api를 호출 삭제 (#65)

--- apps/recruit/src/app/(home)/_components/RecruitmentInactive.tsx
+++ apps/recruit/src/app/(home)/_components/RecruitmentInactive.tsx
@@ -1,13 +1,9 @@
 import RecruitmentLayout from '@/components/layout/RecruitmentLayout';
-import {useRecruitmentStatusQuery} from '@/hooks/queries/useRecruitmentStatus.query';
 
 export const RecruitmentInactive = () => {
-  const {data: recruitmentStatus} = useRecruitmentStatusQuery();
-  const isRecruiting = recruitmentStatus?.isActive ?? false;
-
   return (
     <RecruitmentLayout
-      isRecruiting={isRecruiting}
+      isRecruiting={false}
       backgroundColor='bg-neutral-50'
       visualStripSrc='/images/visual/recruitment-visual-strip-white.webp'
     />
