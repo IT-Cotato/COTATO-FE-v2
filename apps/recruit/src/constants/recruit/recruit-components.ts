fcf25ab7f88175bbc35125ffa2f2d758a5617a6c
ion <delion.dev@gmail.com>
2026-02-18 16:38:48 +0900

    name(recruit): public 이미지 폴더명 변경 (#65)

--- apps/recruit/src/constants/recruit/recruit-components.ts
+++ apps/recruit/src/constants/recruit/recruit-components.ts
@@ -6,17 +6,17 @@ import {
 export const QUALIFICATIONS_CARD_ITEMS = [
   {
     qualification: 'session',
-    illustrationSrc: '/images/qualifications/clock.webp',
+    illustrationSrc: '/images/qualification-card/clock.webp',
     description: '매주 금요일 19시\n정기세션 참여가능한 자',
   },
   {
     qualification: 'grade',
-    illustrationSrc: '/images/qualifications/univ.webp',
+    illustrationSrc: '/images/qualification-card/univ.webp',
     description: '서울, 경기권 대학교\n4학기 이상 수료자',
   },
   {
     qualification: 'department',
-    illustrationSrc: '/images/qualifications/notebook.webp',
+    illustrationSrc: '/images/qualification-card/notebook.webp',
     description: '개발자 지원 시 컴퓨터/IT\n관련학과 주·복수 전공생',
   },
 ];
