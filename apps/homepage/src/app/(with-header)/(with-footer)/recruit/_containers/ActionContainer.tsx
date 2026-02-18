8990c5747aefed72443ae605e24f65717fd61286
ion <delion.dev@gmail.com>
2026-02-18 14:57:45 +0900

    feat(homepage): recruitment 페이지에 ActionContainer 컴포넌트를 추가 (#65)

---
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/_containers/ActionContainer.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import RecruitmentLayout from '@/components/layout/RecruitmentLayout';
+import {Spinner} from '@repo/ui/components/spinner/Spinner';
+// import {useRecruitmentStatusQuery} from '@/hooks/queries/useRecruitmentStatus.query';
+
+// mock
+const isLoading = false;
+const isRecruiting = true;
+
+export const ActionContainer = () => {
+  // const {data: recruitmentStatus, isLoading} = useRecruitmentStatusQuery();
+  // const isRecruiting = recruitmentStatus?.isActive ?? false;
+
+  if (isLoading) {
+    return (
+      <div className='flex h-screen w-full items-center justify-center bg-[#010101]'>
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <RecruitmentLayout
+      isRecruiting={isRecruiting}
+      bgColor='bg-[#010101]'
+      bottomBannerImage='/images/visual/recruitment-visual-strip-black.webp'
+      limitBottomBannerWidth={true}
+    />
+  );
+};
--- apps/homepage/src/app/(with-header)/(with-footer)/recruit/page.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/page.tsx
@@ -1,8 +1,9 @@
+import {ActionContainer} from '@/app/(with-header)/(with-footer)/recruit/_containers/ActionContainer';
+
 export default function RecruitPage() {
   return (
     <section>
-      <></>
-      <></>
+      <ActionContainer />
     </section>
   );
 }
