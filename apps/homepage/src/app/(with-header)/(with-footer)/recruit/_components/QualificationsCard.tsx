2214d182d4e1fe13a40d92ea173b82b5c75f7a9b
ion <delion.dev@gmail.com>
2026-02-18 16:43:01 +0900

    feat(homepage): 자격요건 카드 컴포넌트 및 관련 타입, 상수 추가 (#65)

---
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/_components/QualificationsCard.tsx
@@ -0,0 +1,23 @@
+import {QualificationsCardItem} from '@/types/recruitment/recruitment-components.type';
+import Image from 'next/image';
+
+interface QualificationsCardProps {
+  item: QualificationsCardItem;
+}
+
+export const QualificationsCard = ({item}: QualificationsCardProps) => {
+  return (
+    <div className='flex h-73 w-74.5 shrink-0 flex-col items-center justify-center gap-2.5 rounded-[10px] bg-neutral-50/80 bg-[radial-gradient(50%_50%_at_50%_50%,rgba(255,255,255,0.00)_55.77%,rgba(255,255,255,0.50)_100%)] shadow-[0_6px_15px_0_rgba(0,0,0,0.10)] select-none'>
+      <Image
+        src={item.illustrationSrc}
+        alt='QualificationsCard'
+        draggable={false}
+        width={134}
+        height={134}
+      />
+      <p className='text-h5 text-center wrap-anywhere whitespace-pre-line text-neutral-800'>
+        {item.description}
+      </p>
+    </div>
+  );
+};
---
+++ apps/homepage/src/types/recruitment/recruitment-components.type.ts
@@ -0,0 +1,3 @@
+import {QUALIFICATIONS_CARD_ITEMS} from '@/constants/recruitment/recruitment-components';
+
+export type QualificationsCardItem = (typeof QUALIFICATIONS_CARD_ITEMS)[number];
