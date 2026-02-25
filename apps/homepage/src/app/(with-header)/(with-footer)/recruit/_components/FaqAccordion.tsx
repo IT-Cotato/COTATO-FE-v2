7f43c6b90bfb13411272d00f810be335433cf462
ion <delion.dev@gmail.com>
2026-02-25 23:33:09 +0900

    refactor(homepage): faq 아코디언 아이콘 크기 속성 지정 방법 수정 (#157)

--- apps/homepage/src/app/(with-header)/(with-footer)/recruit/_components/FaqAccordion.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/_components/FaqAccordion.tsx
@@ -33,15 +33,19 @@ export const FaqAccordion = ({item}: FaqAccordionProps) => {
         <div className='relative h-6 w-6'>
           <PlusIcon
             aria-hidden={isOpen}
+            width={24}
+            height={24}
             className={clsx(
-              'absolute inset-0 h-6 w-6 transition-all duration-300',
+              'absolute inset-0 transition-all duration-300',
               isOpen ? 'opacity-0' : 'opacity-100'
             )}
           />
           <MinusIcon
             aria-hidden={!isOpen}
+            width={24}
+            height={24}
             className={clsx(
-              'absolute inset-0 h-6 w-6 transition-all duration-300',
+              'absolute inset-0 transition-all duration-300',
               isOpen ? 'rotate-0 opacity-100' : 'opacity-0'
             )}
           />
