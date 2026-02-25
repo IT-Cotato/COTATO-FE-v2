c8dcd45f2d90721081621d6959ad5c53880b8948
ion <delion.dev@gmail.com>
2026-02-25 23:08:39 +0900

    feat(homepage): 모집 파트 API 연동 및 PositionList 컴포넌트 수정 (#157)

--- apps/homepage/src/app/(with-header)/(with-footer)/recruit/_containers/ContentContainer.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/_containers/ContentContainer.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import Image from 'next/image';
 import {QualificationList} from '@/app/(with-header)/(with-footer)/recruit/_components/QualificationList';
 import {PositionList} from '@/app/(with-header)/(with-footer)/recruit/_components/PositionList';
 import {ActivityList} from '@/app/(with-header)/(with-footer)/recruit/_components/ActivityList';
 import {FaqContainer} from '@/app/(with-header)/(with-footer)/recruit/_containers/FaqContainer';
+import {useRecruitmentNoticeQuery} from '@/hooks/queries/useRecruitments.query';
 
 export const ContentContainer = () => {
+  const {data} = useRecruitmentNoticeQuery();
+
+  const dataPosition = data?.parts;
+
   return (
     <div className='relative min-w-360 overflow-hidden'>
       {/* dimd */}
@@ -33,7 +40,7 @@ export const ContentContainer = () => {
       {/* content */}
       <div className='relative z-1 m-auto flex w-360 flex-col gap-45 px-30.25 pt-73 pb-23'>
         <QualificationList />
-        <PositionList />
+        <PositionList parts={dataPosition} />
         <ActivityList />
         <FaqContainer />
       </div>
