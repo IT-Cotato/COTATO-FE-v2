e8a10b72f2d7f2d7f8fb7a2c92241707a11bc2fa
ion <delion.dev@gmail.com>
2026-02-28 16:02:06 +0900

    feat(homepage): 출석 세션 테이블 데이터 및 스키마 추가 (#204)

---
+++ apps/homepage/src/mocks/admin/mock-admin-attendance.ts
@@ -0,0 +1,72 @@
+export const MOCK_FULL_SESSION_TABLE = [
+  {
+    memberInfo: {
+      memberId: 4,
+      name: '김민아',
+      position: 'FE',
+      generationId: 11,
+    },
+    statistic: {
+      present: 1,
+      late: 0,
+      absent: 0,
+      unauthorizedAbsent: 0,
+    },
+  },
+  {
+    memberInfo: {
+      memberId: 2,
+      name: '박상현',
+      position: 'PM',
+      generationId: 11,
+    },
+    statistic: {
+      present: 0,
+      late: 1,
+      absent: 0,
+      unauthorizedAbsent: 0,
+    },
+  },
+  {
+    memberInfo: {
+      memberId: 1,
+      name: '양희정',
+      position: 'FE',
+      generationId: 11,
+    },
+    statistic: {
+      present: 0,
+      late: 0,
+      absent: 0,
+      unauthorizedAbsent: 0,
+    },
+  },
+  {
+    memberInfo: {
+      memberId: 3,
+      name: '진서영',
+      position: 'PM',
+      generationId: 11,
+    },
+    statistic: {
+      present: 0,
+      late: 0,
+      absent: 0,
+      unauthorizedAbsent: 0,
+    },
+  },
+  {
+    memberInfo: {
+      memberId: 6,
+      name: '하지민',
+      position: 'FE',
+      generationId: 11,
+    },
+    statistic: {
+      present: 0,
+      late: 0,
+      absent: 0,
+      unauthorizedAbsent: 0,
+    },
+  },
+];
--- apps/homepage/src/schemas/admin/attendance.schema.ts
+++ apps/homepage/src/schemas/admin/attendance.schema.ts
@@ -2,5 +2,21 @@ import z from 'zod';
 
 export const AttendancePartSchema = z.enum(['ALL', 'PM', 'DE', 'FE', 'BE']);
 
+export const FullSessionTableRowSchema = z.object({
+  memberInfo: z.object({
+    memberId: z.number(),
+    name: z.string(),
+    position: z.string(),
+    generationId: z.number(),
+  }),
+  statistic: z.object({
+    present: z.number(),
+    late: z.number(),
+    absent: z.number(),
+    unauthorizedAbsent: z.number(),
+  }),
+});
+
 /** 타입 추출 */
 export type AttendancePartType = z.infer<typeof AttendancePartSchema>;
+export type FullSessionTableRowType = z.infer<typeof FullSessionTableRowSchema>;
