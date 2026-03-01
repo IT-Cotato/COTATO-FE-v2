df3c826e42d0b72e52448fd2301f244704fbb163
ion <delion.dev@gmail.com>
2026-03-01 15:04:01 +0900

    feat(homepage): 전체 출석 목록을 위한 쿼리 및 로딩 상태 처리 추가 (#204)

--- apps/homepage/src/app/(with-header)/mypage/admin/attendance/_containers/TableContainer.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/attendance/_containers/TableContainer.tsx
@@ -1,25 +1,38 @@
 'use client';
 
 import {FullSessionTable} from '@/app/(with-header)/mypage/admin/attendance/_components/table/FullSessionTable';
-import {
-  MOCK_FULL_SESSION_TABLE,
-  MOCK_SPECIFIC_SESSION_TABLE,
-} from '@/mocks/admin/mock-admin-attendance';
+import {MOCK_SPECIFIC_SESSION_TABLE} from '@/mocks/admin/mock-admin-attendance';
 import {SpecificSessionTable} from '@/app/(with-header)/mypage/admin/attendance/_components/table/SpecificSessionTable';
 import {useAdminAttendanceStore} from '@/store/useAdminAttendanceStore';
 import {useAttendanceFullRecordQuery} from '@/hooks/queries/useAttendance.queries';
+import {Spinner} from '@repo/ui/components/spinner/Spinner';
 
 export const TableContainer = () => {
-  const {selectedSessionType} = useAdminAttendanceStore();
+  const {selectedGenerationNumber, selectedSessionType} =
+    useAdminAttendanceStore();
 
-  // const {data: fullAttendanceList} = useAttendanceFullRecordQuery()
+  const {data: fullAttendanceList, isLoading: isFullAttendanceLoading} =
+    useAttendanceFullRecordQuery(selectedGenerationNumber ?? 0);
 
   const half = Math.ceil(MOCK_SPECIFIC_SESSION_TABLE.length / 2);
+
+  if (!fullAttendanceList || isFullAttendanceLoading) {
+    return (
+      <div
+        className='flex min-h-100 items-center justify-center'
+        role='status'
+        aria-live='polite'
+        aria-busy='true'>
+        <Spinner />
+        <span className='sr-only'>데이터를 불러오는 중입니다.</span>
+      </div>
+    );
+  }
 
   return (
     <>
       {selectedSessionType === 'FULL' ? (
-        <FullSessionTable items={MOCK_FULL_SESSION_TABLE} />
+        <FullSessionTable items={fullAttendanceList} />
       ) : (
         <div className='flex gap-5'>
           <SpecificSessionTable
