979d9c06b28c91616c46d346ef98b4f8acf19b48
kimminna <kmina121777@naver.com>
2026-03-02 03:50:26 +0900

    style(homepage): 패드 크기 최적화를 위한 로고 속성 xl 크기로 변경 (#223)

--- apps/homepage/src/app/onboarding/page.tsx
+++ apps/homepage/src/app/onboarding/page.tsx
@@ -15,7 +15,7 @@ export default function OnboardingPage() {
         )}
       />
 
-      <div className='absolute inset-0 flex h-0 w-0 items-center overflow-hidden opacity-0 lg:h-full lg:w-full lg:opacity-100'>
+      <div className='absolute inset-0 flex h-0 w-0 items-center overflow-hidden opacity-0 xl:h-full xl:w-full xl:opacity-100'>
         <div className='flex h-full w-1/2 items-center justify-center'>
           <CotatoLogo />
         </div>
