4c3408ed3712f2c37794525dd12396bd1fd32631
kimminna <kmina121777@naver.com>
2026-03-02 22:38:54 +0900

    style(homepage): 활동 일정 소개 섹션 내부 키캡 반응형 구현 (#225)

--- apps/homepage/src/app/(with-header)/(with-footer)/(home)/_components/keycap/BlackPlusKeycap.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/(home)/_components/keycap/BlackPlusKeycap.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export const BlackPlusKeycap = () => {
   return (
     <div
-      className='group relative inline-block h-67 w-67 cursor-pointer rounded-[50px]'
+      className='group relative inline-block h-39 w-39 cursor-pointer rounded-[50px] xl:h-67 xl:w-67'
       role='button'
       tabIndex={0}>
       <Image src='/keycap/black-keycap.svg' alt='' fill unoptimized={true} />
@@ -16,8 +16,8 @@ export const BlackPlusKeycap = () => {
         className='absolute top-0 left-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100'
         unoptimized={true}
       />
-      <SmallLogo className='absolute top-1/2 left-1/2 h-21 w-20 -translate-x-1/2 -translate-y-1/2 text-neutral-500 opacity-100 transition-opacity duration-300 group-hover:opacity-0 group-focus-visible:opacity-0' />
-      <SmallLogoHover className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-visible:opacity-100' />
+      <SmallLogo className='absolute top-1/2 left-1/2 h-12.5 w-12.5 -translate-x-1/2 -translate-y-1/2 text-neutral-500 opacity-100 transition-opacity duration-300 group-hover:opacity-0 group-focus-visible:opacity-0 xl:h-21 xl:w-20' />
+      <SmallLogoHover className='absolute top-1/2 left-1/2 h-22.5 w-22.5 -translate-x-1/2 -translate-y-1/2 text-neutral-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-visible:opacity-100 xl:h-40 xl:w-40' />
     </div>
   );
 };
--- apps/homepage/src/app/(with-header)/(with-footer)/(home)/_components/keycap/ScheduleKeycap.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/(home)/_components/keycap/ScheduleKeycap.tsx
@@ -22,7 +22,7 @@ export const ScheduleKeycap = ({
 }: ScheduleKeycapProps) => {
   return (
     <div
-      className={`group relative inline-block h-67 w-67 cursor-pointer overflow-hidden rounded-[50px] ${className}`}
+      className={`group relative inline-block h-39 w-39 cursor-pointer overflow-hidden rounded-[27px] xl:h-67 xl:w-67 xl:rounded-[50px] ${className}`}
       role='button'
       tabIndex={0}>
       <Image
@@ -43,11 +43,11 @@ export const ScheduleKeycap = ({
         <div className='absolute inset-0 bg-black/20' />
       </div>
       <span
-        className={`text-h2 absolute top-16.5 left-16.5 z-10 font-bold transition-colors group-hover:text-white ${titleColor} group-focus-visible:text-white`}>
+        className={`text-h3 xl:text-h2 absolute top-7.75 left-8.75 z-10 font-semibold transition-colors group-hover:text-white xl:top-16.5 xl:left-16.5 ${titleColor} group-focus-visible:text-white`}>
         {title}
       </span>
       {subTitle && (
-        <span className='text-h4 absolute top-30.5 left-16.5 z-10 text-neutral-100 opacity-0 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100'>
+        <span className='text-h5 xl:text-h4 absolute bottom-[27.5px] left-8.75 z-10 text-neutral-100 opacity-0 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100 xl:top-30.5 xl:left-16.5'>
           {subTitle}
         </span>
       )}
