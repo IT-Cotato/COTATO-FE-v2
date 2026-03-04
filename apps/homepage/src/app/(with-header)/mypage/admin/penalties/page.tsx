758fe5940cd007fa12f2c07d3b463f6e61b57deb
ion <delion.dev@gmail.com>
2026-03-04 17:12:49 +0900

    feat(homepage): 상벌점 관리 페이지에 드롭다운 컨테이너 추가 (#205)

--- apps/homepage/src/app/(with-header)/mypage/admin/penalties/page.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/penalties/page.tsx
@@ -1,3 +1,12 @@
+import {DropdownContainer} from '@/app/(with-header)/mypage/admin/penalties/_containers/DropdownContainer';
+
 export default function AdminPenaltiesPage() {
-  return <div>상벌점 관리</div>;
+  return (
+    <section className='px-11.25'>
+      <div className='flex min-w-275 flex-col gap-4.5 py-13.5'>
+        <h1 className='text-h2 text-neutral-800'>상벌점 관리</h1>
+        <DropdownContainer />
+      </div>
+    </section>
+  );
 }
