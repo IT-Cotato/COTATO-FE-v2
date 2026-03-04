e866f39be2f2a485bf07da22e951491ad351fbb5
ion <delion.dev@gmail.com>
2026-03-04 19:17:36 +0900

    feat(homepage): 정렬 타입 추가 (#205)

--- apps/homepage/src/types/mypage/admin/penalties/penalties.type.ts
+++ apps/homepage/src/types/mypage/admin/penalties/penalties.type.ts
@@ -2,3 +2,5 @@ import {PENALTY_FULL_TABLE_HEADER} from '@/constants/admin/admin';
 
 export type PenaltyFullTableKey =
   (typeof PENALTY_FULL_TABLE_HEADER)[number]['key'];
+
+export type SortDirection = 'ASC' | 'DESC';
