ed866b5c87f043400ef65343534099aed2717130
ion <delion.dev@gmail.com>
2026-03-04 23:07:19 +0900

    refactor(homepage): useDebounce 훅 적용을 위한 SpecificSessionRow 컴포넌트 분리 (#205)

---
+++ apps/homepage/src/app/(with-header)/mypage/admin/penalties/_components/row/SpecificSessionRow.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import {
+  ATTENDANCE_STATUS_CONFIG,
+  BEER_NETWORKING_ATTENDANCE_CONFIG,
+} from '@/constants/admin/admin';
+import {SpecificSessionTableRowType} from '@/schemas/admin/admin-penalties.schema';
+import {StatusChip} from '@repo/ui/components/chip/StatusChip';
+import {useDebounce} from '@repo/ui/hooks/useDebounce';
+import {useEffect, useState} from 'react';
+
+interface SpecificSessionRowProps {
+  row: SpecificSessionTableRowType;
+  onClickBeerNetworkingChip: (memberId: number, participated: boolean) => void;
+  isUpdatingBeerNetworkingChip: boolean;
+  onChangeExtraMinusPoint: (memberId: number, extraMinusPoint: number) => void;
+}
+
+export const SpecificSessionRow = ({
+  row,
+  onClickBeerNetworkingChip,
+  isUpdatingBeerNetworkingChip,
+  onChangeExtraMinusPoint,
+}: SpecificSessionRowProps) => {
+  const [value, setValue] = useState(row.extraMinusPoint);
+
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    setValue(row.extraMinusPoint);
+  }, [row.extraMinusPoint]);
+
+  useEffect(() => {
+    if (debouncedValue !== row.extraMinusPoint) {
+      onChangeExtraMinusPoint(row.memberId, debouncedValue);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue]);
+
+  return (
+    <tr className='text-body-l-sb text-center text-neutral-600'>
+      <td className='truncate px-3 py-4'>{row.name}</td>
+
+      <td className='truncate px-3 py-4'>
+        <StatusChip
+          value={row.attendanceResult}
+          config={ATTENDANCE_STATUS_CONFIG}
+          isActive
+          disabled
+        />
+      </td>
+
+      <td className='px-3 py-4'>
+        <StatusChip
+          value={row.beerNetworkingParticipated ? 'PRESENT' : 'ABSENT'}
+          config={BEER_NETWORKING_ATTENDANCE_CONFIG}
+          isActive
+          disabled={isUpdatingBeerNetworkingChip}
+          onClick={() =>
+            onClickBeerNetworkingChip(
+              row.memberId,
+              !row.beerNetworkingParticipated
+            )
+          }
+        />
+      </td>
+
+      <td className='px-3 py-4'>
+        <input
+          type='number'
+          aria-label='기타 벌점'
+          value={value}
+          onChange={(e) => setValue(Number(e.target.value))}
+          className='text-body-l-sb w-full [appearance:textfield] bg-transparent text-center font-normal outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none'
+        />
+      </td>
+    </tr>
+  );
+};
--- apps/homepage/src/app/(with-header)/mypage/admin/penalties/_components/table/SpecificSessionTable.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/penalties/_components/table/SpecificSessionTable.tsx
@@ -1,10 +1,6 @@
-import {
-  ATTENDANCE_STATUS_CONFIG,
-  BEER_NETWORKING_ATTENDANCE_CONFIG,
-  PENALTY_SPECIFIC_TABLE_HEADER,
-} from '@/constants/admin/admin';
+import {PENALTY_SPECIFIC_TABLE_HEADER} from '@/constants/admin/admin';
 import {SpecificSessionTableRowType} from '@/schemas/admin/admin-penalties.schema';
-import {StatusChip} from '@repo/ui/components/chip/StatusChip';
+import {SpecificSessionRow} from '@/app/(with-header)/mypage/admin/penalties/_components//row/SpecificSessionRow';
 
 interface SpecificSessionTableProps {
   items: SpecificSessionTableRowType[];
@@ -45,48 +41,13 @@ export const SpecificSessionTable = ({
           </tr>
         ) : (
           items.map((row) => (
-            <tr
+            <SpecificSessionRow
               key={row.memberId}
-              className='text-body-l-sb text-center text-neutral-600'>
-              <td className='truncate px-3 py-4'>{row.name}</td>
-              <td className='truncate px-3 py-4'>
-                <StatusChip
-                  value={row.attendanceResult}
-                  config={ATTENDANCE_STATUS_CONFIG}
-                  isActive={true}
-                  disabled={true}
-                />
-              </td>
-              <td className='px-3 py-4'>
-                <StatusChip
-                  value={row.beerNetworkingParticipated ? 'PRESENT' : 'ABSENT'}
-                  config={BEER_NETWORKING_ATTENDANCE_CONFIG}
-                  isActive={true}
-                  disabled={isUpdatingBeerNetworkingChip}
-                  onClick={() =>
-                    onClickBeerNetworkingChip(
-                      row.memberId,
-                      !row.beerNetworkingParticipated
-                    )
-                  }
-                />
-              </td>
-              <td className='px-3 py-4'>
-                <input
-                  type='number'
-                  placeholder=''
-                  aria-label='기타 벌점'
-                  value={row.extraMinusPoint}
-                  onChange={(e) =>
-                    onChangeExtraMinusPoint(
-                      row.memberId,
-                      Number(e.target.value)
-                    )
-                  }
-                  className='text-body-l-sb w-full [appearance:textfield] bg-transparent text-center font-normal outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none'
-                />
-              </td>
-            </tr>
+              row={row}
+              onClickBeerNetworkingChip={onClickBeerNetworkingChip}
+              isUpdatingBeerNetworkingChip={isUpdatingBeerNetworkingChip}
+              onChangeExtraMinusPoint={onChangeExtraMinusPoint}
+            />
           ))
         )}
       </tbody>
