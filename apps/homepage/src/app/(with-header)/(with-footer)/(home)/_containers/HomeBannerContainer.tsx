8e01ad0a1436aec19b2964f4fcc660cc6b5ce09b
kimminna <kmina121777@naver.com>
2026-03-07 23:07:47 +0900

    style(homepage): 배너 반응형 xl 쿼리로 수정 (#225)

--- apps/homepage/src/app/(with-header)/(with-footer)/(home)/_containers/HomeBannerContainer.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/(home)/_containers/HomeBannerContainer.tsx
@@ -26,13 +26,13 @@ export const HomeBannerContainer = () => {
       className='group relative flex h-screen w-full cursor-pointer items-center overflow-hidden bg-black'
       aria-label='홈 배너'
       onClick={toggleOverlay}>
-      <div className='absolute inset-0 translate-y-0 transition-all duration-500 lg:px-12.5 lg:group-hover:translate-y-4'>
+      <div className='absolute inset-0 translate-y-0 transition-all duration-500 xl:px-12.5 xl:group-hover:translate-y-4'>
         <div className='relative h-full w-full overflow-hidden rounded-2xl'>
           <Image
             src='/keycap/background-keycap.svg'
             alt=''
             fill
-            className='object-cover object-[40%_center] md:object-center lg:object-contain'
+            className='object-cover object-[40%_center] md:object-center xl:object-contain'
             priority
             aria-hidden='true'
           />
@@ -44,7 +44,7 @@ export const HomeBannerContainer = () => {
           'absolute inset-0 z-10 transition-opacity duration-500',
           'bg-black/40 backdrop-blur-[6px]',
           isOverlayHidden ? 'opacity-0' : 'opacity-100',
-          'lg:opacity-0 lg:group-hover:bg-black/60 lg:group-hover:opacity-100 lg:group-hover:backdrop-blur-[6px]'
+          'xl:opacity-0 xl:backdrop-blur-none xl:group-hover:bg-black/60 xl:group-hover:opacity-100'
         )}
         aria-hidden='true'
       />
@@ -53,7 +53,7 @@ export const HomeBannerContainer = () => {
         className={clsx(
           'absolute top-1/3 left-1/2 z-20 flex -translate-x-1/2 -translate-y-2/3 flex-col items-center transition-opacity duration-300',
           isOverlayHidden ? 'opacity-0' : 'opacity-100',
-          'lg:opacity-0 lg:group-hover:opacity-100'
+          'xl:opacity-0 xl:group-hover:opacity-100'
         )}>
         <CotatoLogo />
       </div>
@@ -62,7 +62,7 @@ export const HomeBannerContainer = () => {
         className={clsx(
           'absolute bottom-60 left-1/2 z-20 -translate-x-1/2 transition-all duration-300',
           isOverlayHidden ? 'opacity-0' : 'opacity-100',
-          'lg:opacity-0 lg:group-hover:opacity-100'
+          'xl:opacity-0 xl:group-hover:opacity-100'
         )}>
         <button
           onClick={scrollToNextSection}
