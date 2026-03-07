2dfa20d5d834a5172aa08ddb6f406a07b796415a
kimminna <kmina121777@naver.com>
2026-03-07 23:21:50 +0900

    style(root): 코테이토 로고 반응형 쿼리 md로 변경 (#225)

--- packages/ui/src/components/logo/CotatoLogo.tsx
+++ packages/ui/src/components/logo/CotatoLogo.tsx
@@ -3,13 +3,13 @@ import MainLogo from '../../assets/main-logo/main-logo.svg';
 export const CotatoLogo = () => {
   return (
     <div className='flex flex-col items-center gap-9.5'>
-      <p className='text-h5 sm:text-h4 bg-linear-to-r from-[#F89202] via-[#F89202] to-[#9E9E9E] bg-clip-text font-bold whitespace-nowrap text-transparent'>
+      <p className='text-h5 md:text-h4 bg-linear-to-r from-[#F89202] via-[#F89202] to-[#9E9E9E] bg-clip-text font-bold whitespace-nowrap text-transparent'>
         COde Together, Arrive TOgether
       </p>
       <MainLogo
         role='img'
         aria-label='COTATO 로고'
-        className='h-auto w-[189px] sm:w-auto'
+        className='h-auto w-[189px] md:w-auto'
       />
     </div>
   );
