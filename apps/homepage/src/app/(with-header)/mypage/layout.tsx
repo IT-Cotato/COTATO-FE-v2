87730f0fa9a5941bdc3eda2a2772536a4be554a3
choyeon2e <cho1428hee@naver.com>
2026-03-12 00:57:19 +0900

    feat(homepage): 마이페이지 모바일용 nav 컴포넌트 구현 및 레이아웃 구조 변경 (#273)

--- apps/homepage/src/app/(with-header)/mypage/layout.tsx
+++ apps/homepage/src/app/(with-header)/mypage/layout.tsx
@@ -1,4 +1,4 @@
-import {ProtectedRoute} from '@/components/auth/ProtectedRoute';
+import {MyPageClientMobileWrapper} from '@/app/(with-header)/mypage/_containers/MyPageClientMobileWrapper';
 import {SideBarContainer} from '@/components/sidebar/SideBarContainer';
 
 export default function MyPageLayout({
@@ -7,13 +7,13 @@ export default function MyPageLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ProtectedRoute>
-      <section className='flex min-h-screen w-full min-w-360 flex-row'>
+    <MyPageClientMobileWrapper>
+      <section className='flex min-h-screen w-full min-w-360 flex-col md:flex-row'>
         <aside className='z-sidebar sticky left-0 hidden bg-neutral-50 md:block'>
           <SideBarContainer />
         </aside>
         <main className='min-w-0 flex-1'>{children}</main>
       </section>
-    </ProtectedRoute>
+    </MyPageClientMobileWrapper>
   );
 }
