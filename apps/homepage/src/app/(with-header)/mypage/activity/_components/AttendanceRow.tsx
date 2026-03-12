7775010d848b05e79bd6f185df945eb3741917ba
choyeon2e <cho1428hee@naver.com>
2026-03-12 22:40:15 +0900

    ui(homepage): 하단 월별 출석기록 모바일뷰 구현 (#276)

--- apps/homepage/src/app/(with-header)/mypage/activity/_components/AttendanceRow.tsx
+++ apps/homepage/src/app/(with-header)/mypage/activity/_components/AttendanceRow.tsx
@@ -16,10 +16,10 @@ export const AttendanceRows = ({data}: {data: MemberAttendResponse[]}) => (
             {item.sessionType === 'ALL' && '대면/비대면 혼용 세션'}
             {item.sessionType === 'NO_ATTEND' && '출석을 진행하지 않는 세션'}
           </td>
-          <td className='flex h-12.25 items-center justify-center py-[8.5px]'>
+          <td className='my-0.5 flex h-7 items-center justify-center md:h-12.25 md:py-[8.5px]'>
             <span
               className={clsx(
-                'text-body-m-sb shadow-default flex h-full w-18.75 items-center justify-center rounded-[10px] text-white',
+                'md:text-body-m-sb text-body-m shadow-mobile md:shadow-default flex h-full w-14.5 items-center justify-center rounded-[10px] text-white md:w-18.75',
                 status?.className || 'bg-neutral-400'
               )}>
               {status?.label || '기록없음'}
