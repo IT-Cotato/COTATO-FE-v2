db00dd162341b541a53a61a8d1b3e398b1b23ba7
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-03-13 02:40:09 +0900

    chore(homepage): 접근성 개선 (#284)

--- apps/homepage/src/app/(with-header)/mypage/admin/recruit/page.tsx
+++ apps/homepage/src/app/(with-header)/mypage/admin/recruit/page.tsx
@@ -5,7 +5,9 @@ export default function AdminRecruitPage() {
   return (
     <section className='flex flex-col px-6 pt-10 md:p-12.5'>
       <div className='flex flex-col gap-6.75 md:min-w-275'>
-        <h1 className='text-h2 hidden md:block'>리크루트 세팅</h1>
+        <h1 className='text-h2 sr-only md:not-sr-only md:block'>
+          리크루트 세팅
+        </h1>
         <SuspenseWrapper>
           <RecruitLinkCard />
         </SuspenseWrapper>
