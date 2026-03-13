2dc47580b83fe6e5831d673e9789e92d1313063e
choyeon2e <cho1428hee@naver.com>
2026-03-13 22:30:19 +0900

    ui(homepage): 개인정보처리방침 모바일뷰 구현 (#294)

--- apps/homepage/src/app/(with-header)/mypage/terms/_components/PrivacyPolicy.tsx
+++ apps/homepage/src/app/(with-header)/mypage/terms/_components/PrivacyPolicy.tsx
@@ -5,18 +5,22 @@ import {
 
 export const PrivacyPolicy = () => {
   return (
-    <div className='flex h-full w-full flex-col items-start gap-5 py-[38.5px] pl-10 text-neutral-800'>
-      <h4 className='text-h4'>코테이토(COTATO) 개인정보 처리방침</h4>
-      <div className='text-body-l flex flex-col pb-2'>
+    <div className='flex h-full w-full flex-col items-start gap-5 px-5 py-5 text-neutral-800 lg:px-10 lg:py-[38.5px]'>
+      <h4 className='text-h5 lg:text-h4 font-bold'>
+        코테이토(COTATO) 개인정보 처리방침
+      </h4>
+      <div className='text-body-l flex flex-col lg:pb-2'>
         {PRIVACY_POLICY_INTRO.content}
       </div>
       {PRIVACY_POLICY.map((section) => (
         <div key={section.title} className='flex flex-col gap-1'>
-          <span className='text-body-l-sb'>{section.title}</span>
+          <span className='text-body-l-b lg:text-body-l-sb'>
+            {section.title}
+          </span>
           {section.content}
         </div>
       ))}
-      <div className='h-[38.5px] w-full shrink-0' aria-hidden='true' />
+      <div className='w-full shrink-0 lg:h-[38.5px]' aria-hidden='true' />
     </div>
   );
 };
