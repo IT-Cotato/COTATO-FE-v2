f4acafaa745af2db1b8ec124f89dd74ed79cfa5c
choyeon2e <cho1428hee@naver.com>
2026-03-13 22:10:07 +0900

    ui(homepage): 이용약관 페이지 관련 전체 모바일 레이아웃 구성 (#294)

--- apps/homepage/src/app/(with-header)/mypage/terms/_components/ServiceTerms.tsx
+++ apps/homepage/src/app/(with-header)/mypage/terms/_components/ServiceTerms.tsx
@@ -5,18 +5,22 @@ import {
 
 export const ServiceTerms = () => {
   return (
-    <div className='flex h-full w-full flex-col items-start gap-5 py-[38.5px] pl-10 text-neutral-800'>
-      <h4 className='text-h4'>코테이토(COTATO) 서비스 이용약관</h4>
-      <div className='text-body-l flex flex-col pb-2'>
+    <div className='flex h-full w-full flex-col items-start gap-5 px-5 py-5 text-neutral-800 lg:px-10 lg:py-[38.5px]'>
+      <h4 className='text-h5 lg:text-h4 font-bold'>
+        코테이토(COTATO) 서비스 이용약관
+      </h4>
+      <div className='text-body-l flex flex-col lg:pb-2'>
         {SERVICE_TERMS_INTRO.content}
       </div>
       {SERVICE_TERMS.map((section) => (
         <div key={section.title} className='flex flex-col gap-1'>
-          <span className='text-body-l-sb'>{section.title}</span>
+          <span className='text-body-l-b lg:text-body-l-sb'>
+            {section.title}
+          </span>
           {section.content}
         </div>
       ))}
-      <div className='h-[38.5px] w-full shrink-0' aria-hidden='true' />
+      <div className='w-full shrink-0 lg:h-[38.5px]' aria-hidden='true' />
     </div>
   );
 };
