961b69f0b5176e8b80f30cc4894ad0fb79cb1090
choyeon2e <cho1428hee@naver.com>
2026-03-15 16:39:44 +0900

    ui(homepage): 프로젝트 전체 레이아웃 모바일뷰 구현 및 heromainbanner 구조 수정 및 모바일 구현 (#307)

--- apps/homepage/src/app/(with-header)/(with-footer)/project/layout.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/project/layout.tsx
@@ -4,18 +4,23 @@ import HeroBanner from '@/assets/backgrounds/banner/hero-main.webp';
 
 export default function ProjectLayout({children}: {children: React.ReactNode}) {
   return (
-    <section className='flex min-h-screen min-w-360 flex-col items-center'>
+    <section className='flex min-h-screen flex-col items-center lg:min-w-360'>
       <HeroMainBanner
-        heading='함께 만들어 도착한, COTATO의 프로젝트를 만나보세요'
+        paddingVertical={true}
+        heading={
+          <>
+            함께 만들어 도착한,
+            <br className='lg:hidden' /> COTATO의 프로젝트를 만나보세요
+          </>
+        }
         subheading='COde Together, Arrive TOgether'
-        paddingVertical={76}
         bannerImage={
           <Image
             src={HeroBanner}
             alt='Hero Banner'
             fill
             priority
-            className='object-cover object-center'
+            className='object-cover object-left lg:object-center'
           />
         }
       />
