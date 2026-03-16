6d04f6991c2bbd8328be6ccc13daf225a09e8bdc
choyeon2e <cho1428hee@naver.com>
2026-03-16 17:27:31 +0900

    ui(homepage): 모바일에서의 멤버 삭제를 위해 memberchip의 x 버튼을 모바일에서 호버가 아닌 항상 렌더링으로 변경 (#310)

--- apps/homepage/src/app/(with-header)/(with-footer)/project/add-project/_components/MemberChip.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/project/add-project/_components/MemberChip.tsx
@@ -39,8 +39,8 @@ export const MemberChip = ({
 
   return (
     <div
-      className={`group relative flex h-8 items-center rounded-[20px] px-3.75 text-white transition-all lg:px-5.75 lg:py-1.25 ${bgColor} ${
-        isEditing ? 'pr-12' : 'focus-within:pr-12 hover:pr-12'
+      className={`group relative flex h-8 items-center rounded-[20px] px-3.75 pr-10 text-white transition-all lg:px-5.75 lg:py-1.25 ${bgColor} ${
+        isEditing ? 'lg:pr-12' : 'lg:focus-within:pr-12 lg:hover:pr-12'
       }`}
       role='group'
       aria-label={`팀원 ${name}`}>
@@ -74,11 +74,7 @@ export const MemberChip = ({
         type='button'
         onClick={onDelete}
         aria-label={`${name} 삭제`}
-        className={`absolute right-5 ${
-          isEditing
-            ? 'block'
-            : 'hidden group-focus-within:block group-hover:block'
-        }`}>
+        className={`absolute right-3 lg:right-5 ${isEditing ? 'block' : 'block lg:hidden lg:group-focus-within:block lg:group-hover:block'} }`}>
         <XIcon className='h-3 w-3 text-white' aria-hidden='true' />
       </button>
     </div>
