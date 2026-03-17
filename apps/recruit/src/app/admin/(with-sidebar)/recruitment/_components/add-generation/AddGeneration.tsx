50a060c218af9e68d9c60cbbe7d127c3e5c40df0
choyeon2e <cho1428hee@naver.com>
2026-03-17 20:44:12 +0900

    ui(homepage): 기수 추가하기 관련 컴포넌트 모바일UI 구현 (#316)

--- apps/recruit/src/app/admin/(with-sidebar)/recruitment/_components/add-generation/AddGeneration.tsx
+++ apps/recruit/src/app/admin/(with-sidebar)/recruitment/_components/add-generation/AddGeneration.tsx
@@ -2,8 +2,10 @@ import {AddGenerationContainer} from '@/app/admin/(with-sidebar)/recruitment/_co
 
 export const AddGeneration = () => {
   return (
-    <section className='flex w-full flex-col gap-5'>
-      <h2 className='text-h4'>기수 추가하기</h2>
+    <section className='flex w-full flex-col gap-2.5 lg:gap-5'>
+      <h2 className='text-h5 lg:text-h4 px-2.5 font-bold lg:px-0'>
+        기수 추가하기
+      </h2>
       <AddGenerationContainer />
     </section>
   );
