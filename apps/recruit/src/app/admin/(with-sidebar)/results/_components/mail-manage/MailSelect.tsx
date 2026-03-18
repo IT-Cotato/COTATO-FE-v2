78ea8f91165f4ae36582f27e85b4925e6f004cc0
choyeon2e <cho1428hee@naver.com>
2026-03-18 23:38:13 +0900

    ui(recruit): 메일 선택 탭 모바일 UI 구현 (#322)

--- apps/recruit/src/app/admin/(with-sidebar)/results/_components/mail-manage/MailSelect.tsx
+++ apps/recruit/src/app/admin/(with-sidebar)/results/_components/mail-manage/MailSelect.tsx
@@ -12,7 +12,7 @@ interface MailSelectProps {
 export const MailSelect = ({activeTab, onTabChange}: MailSelectProps) => {
   return (
     <nav aria-label='합격자 관리 - 메일 전송 탭'>
-      <ul className='flex gap-12.5'>
+      <ul className='flex justify-between gap-8 lg:justify-start lg:gap-12.5'>
         {mailTabs.map((tab) => {
           const isActive = activeTab === tab;
           return (
@@ -23,7 +23,10 @@ export const MailSelect = ({activeTab, onTabChange}: MailSelectProps) => {
                 borderRadius={0}
                 backgroundColor='white'
                 textColor={isActive ? 'neutral-800' : 'neutral-500'}
-                className={clsx('px-0', 'border-none')}
+                className={clsx(
+                  'border-none px-0',
+                  'text-body-l-b! lg:text-h5!'
+                )}
                 onClick={() => onTabChange(tab)}
               />
             </li>
