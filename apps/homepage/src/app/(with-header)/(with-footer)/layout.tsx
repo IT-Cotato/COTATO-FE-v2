1c754905c9a5d24747cc348d949d37676c171e7f
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-04-03 17:07:48 +0900

    fix(homepage): 변경된 Footer props 반영 (#333)

--- apps/homepage/src/app/(with-header)/(with-footer)/layout.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/layout.tsx
@@ -1,4 +1,5 @@
 import {Footer} from '@repo/ui/components/layout/footer/Footer';
+import {TERMS_LINK} from '@repo/ui/constants/terms-link';
 
 export default function WithFooterLayout({
   children,
@@ -8,7 +9,7 @@ export default function WithFooterLayout({
   return (
     <div>
       <main className='w-full flex-1'>{children}</main>
-      <Footer isRecruit={false} />
+      <Footer termsHref={TERMS_LINK.homepage} termsText='서비스 이용약관' />
     </div>
   );
 }
