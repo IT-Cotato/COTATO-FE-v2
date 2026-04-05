f7c0b21e684ab3496c6ab69b5818a135197a40c9
ion <delion.dev@gmail.com>
2026-04-05 15:38:48 +0900

    chore(homepage): 더미 데이터 파일 추가 및 ContentContainer에서 사용 (#298)

---
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/dummy.ts
@@ -0,0 +1,66 @@
+import {
+  ActivityCardType,
+  PositionCardType,
+} from '@/schemas/recruit/recruit.schema';
+
+export const PositionCardsDummy: PositionCardType[] = [
+  {
+    short: 'PM',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'DE',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'FE',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'BE',
+    name: 'string',
+    detail: 'string',
+  },
+];
+
+export const ActivityCardsDummy: ActivityCardType[] = [
+  {
+    id: 0,
+    short: 'OT',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 1,
+    short: 'SESSION',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 2,
+    short: 'MT',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 3,
+    short: 'DEVTALK',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 4,
+    short: 'COKERTHON',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 5,
+    short: 'DEMODAY',
+    name: 'string',
+    date: 'string',
+  },
+];
