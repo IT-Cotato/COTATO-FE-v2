6274528ec11259374c68313c8f24d606fb726319
ion <delion.dev@gmail.com>
2026-04-05 16:28:30 +0900

    feat(homepage): faq container 모바일 뷰 스타일 추가 (#298)

--- apps/homepage/src/app/(with-header)/(with-footer)/recruit/_containers/FaqContainer.tsx
+++ apps/homepage/src/app/(with-header)/(with-footer)/recruit/_containers/FaqContainer.tsx
@@ -12,38 +12,57 @@ export const FaqContainer = () => {
   const {data} = useFaqQuery(isActive);
 
   return (
-    <div className='flex flex-col gap-30'>
-      <div className='flex flex-col gap-17.5'>
-        <p className='text-h2 text-center text-neutral-800'>FAQ</p>
-        <div className='flex gap-22 px-8.75'>
-          <FaqSideBar activatedMenu={isActive} onActive={setIsActive} />
-          <div className='flex flex-1 flex-col gap-6.25'>
-            {data?.map((item) => (
-              <FaqAccordion key={item.id} item={item} />
-            ))}
-          </div>
+    <div className='px-6 py-12.5 lg:px-0 lg:py-0'>
+      <p className='text-h2 mb-17.5 hidden text-center text-neutral-800 lg:block'>
+        FAQ
+      </p>
+
+      <div className='mx-auto mb-12.5 flex max-w-5xl flex-col gap-7.5 lg:mb-30 lg:flex-row lg:gap-17.5'>
+        <FaqSideBar activatedMenu={isActive} onActive={setIsActive} />
+        <div className='flex flex-1 flex-col gap-6.25'>
+          {data?.map((item) => (
+            <FaqAccordion key={item.id} item={item} />
+          ))}
         </div>
       </div>
 
       <div>
-        <p className='text-h4 mb-4 text-center text-neutral-600'>
+        <p className='text-body-l lg:text-h4 mb-4 text-center text-neutral-600'>
           더 자세한 궁금한 내용이 있다면?
         </p>
-        <Button
-          label='카카오톡 채널 문의하기'
-          labelTypo='h3'
-          width={393}
-          height={74}
-          backgroundColor='primary'
-          textColor='neutral-50'
-          onClick={() =>
-            window.open(
-              'https://pf.kakao.com/_LQLyG',
-              '_blank',
-              'noopener,noreferrer'
-            )
-          }
-        />
+        <div className='hidden lg:block'>
+          <Button
+            label='카카오톡 채널 문의하기'
+            labelTypo='h3'
+            width={393}
+            height={74}
+            backgroundColor='primary'
+            textColor='neutral-50'
+            onClick={() =>
+              window.open(
+                'https://pf.kakao.com/_LQLyG',
+                '_blank',
+                'noopener,noreferrer'
+              )
+            }
+          />
+        </div>
+        <div className='lg:hidden'>
+          <Button
+            label='카카오톡 채널 문의하기'
+            labelTypo='body_l_sb'
+            width={327}
+            backgroundColor='primary'
+            textColor='neutral-800'
+            onClick={() =>
+              window.open(
+                'https://pf.kakao.com/_LQLyG',
+                '_blank',
+                'noopener,noreferrer'
+              )
+            }
+          />
+        </div>
       </div>
     </div>
   );
