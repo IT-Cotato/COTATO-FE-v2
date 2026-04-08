6b0bae42a7984991c59acaffb9f03c223f45acce
ion <delion.dev@gmail.com>
2026-04-08 21:16:33 +0900

    chore(recruit): 더미 데이터 추가 - Timeline, Position, Activity 카드 (#340)

---
+++ apps/recruit/src/app/recruit/dummy.ts
@@ -0,0 +1,76 @@
+import {
+  ActivityCardType,
+  PositionCardType,
+  TimelineType,
+} from '@/schemas/recruit/recruit.schema';
+
+export const TimelineDummy: TimelineType[] = [
+  {title: 'test1', date: '2026.01.01'},
+  {title: 'test2', date: '2026.01.02'},
+  {title: 'test3', date: '2026.01.03'},
+  {title: 'test4', date: '2026.01.04'},
+  {title: 'test5', date: '2026.01.05'},
+  {title: 'test6', date: '2026.01.06'},
+];
+
+export const PositionCardsDummy: PositionCardType[] = [
+  {
+    short: 'PM',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'DE',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'FE',
+    name: 'string',
+    detail: 'string',
+  },
+  {
+    short: 'BE',
+    name: 'string',
+    detail: 'string',
+  },
+];
+
+export const ActivityCardsDummy: ActivityCardType[] = [
+  {
+    id: 0,
+    short: 'OT',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 1,
+    short: 'SESSION',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 2,
+    short: 'MT',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 3,
+    short: 'DEVTALK',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 4,
+    short: 'COKERTHON',
+    name: 'string',
+    date: 'string',
+  },
+  {
+    id: 5,
+    short: 'DEMODAY',
+    name: 'string',
+    date: 'string',
+  },
+];
