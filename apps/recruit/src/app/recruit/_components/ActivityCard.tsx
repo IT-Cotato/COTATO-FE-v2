ce30d2e58adb8a1227510e201b57f8209131752b
ion <delion.dev@gmail.com>
2026-04-08 21:23:40 +0900

    feat(recruit): 모바일 ActivityCard에 터치 이벤트 추가 및 ActivityList 컴포넌트 생성 (#340)

--- apps/recruit/src/app/recruit/_components/ActivityCard.tsx
+++ apps/recruit/src/app/recruit/_components/ActivityCard.tsx
@@ -1,15 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import clsx from 'clsx';
 import {ActivityCardType} from '@/schemas/recruit/recruit.schema';
 import {ACTIVITY_CARD_STYLES} from '@/constants/recruit/recruit-components';
+import {useState} from 'react';
 
 interface ActivityCardProps {
   item: ActivityCardType;
 }
 
 export const ActivityCard = ({item}: ActivityCardProps) => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className='group relative h-68.5 w-96.5 shrink-0 overflow-hidden rounded-[10px] bg-neutral-800 px-10 py-6 shadow-[0_6px_15px_0_rgba(0,0,0,0.1)] select-none'>
+    <div
+      className='group relative h-57.5 w-81.75 shrink-0 overflow-hidden rounded-[10px] bg-neutral-800 px-10 py-6 shadow-[0_6px_15px_0_rgba(0,0,0,0.1)] select-none lg:h-68.5 lg:w-96.5'
+      onTouchStart={() => setIsActive(true)}
+      onTouchEnd={() => setIsActive(false)}>
       <Image
         src={ACTIVITY_CARD_STYLES[item.short].coverImageUrl}
         alt=''
@@ -17,7 +25,8 @@ export const ActivityCard = ({item}: ActivityCardProps) => {
         sizes='386px'
         draggable={false}
         className={clsx(
-          'object-cover object-center transition-opacity duration-300 group-hover:opacity-0',
+          'object-cover object-center transition-opacity duration-300',
+          isActive ? 'opacity-0' : 'group-hover:opacity-0',
           ACTIVITY_CARD_STYLES[item.short].style
         )}
       />
@@ -28,19 +37,35 @@ export const ActivityCard = ({item}: ActivityCardProps) => {
         fill={true}
         sizes='386px'
         draggable={false}
-        className='object-cover object-center opacity-0 transition-opacity duration-300 group-hover:opacity-100'
+        className={clsx(
+          'object-cover object-center opacity-0 transition-opacity duration-300',
+          isActive ? 'opacity-100' : 'group-hover:opacity-100'
+        )}
       />
 
       <div
         aria-hidden={true}
-        className='absolute inset-0 z-10 bg-linear-to-b from-[#000000]/90 from-0% to-transparent to-48% transition-all duration-300 group-hover:from-white group-hover:to-58%'
+        className={clsx(
+          'absolute inset-0 z-10 bg-linear-to-b from-[#000000]/90 from-0% to-transparent to-48% transition-all duration-300',
+          isActive
+            ? 'from-white to-58%'
+            : 'group-hover:from-white group-hover:to-58%'
+        )}
       />
 
       <div className='relative z-20 flex justify-between'>
-        <p className='text-h4 text-neutral-50 transition-colors duration-300 group-hover:text-neutral-800'>
+        <p
+          className={clsx(
+            'text-h4 text-neutral-50 transition-colors duration-300',
+            isActive ? 'text-neutral-800' : 'group-hover:text-neutral-800'
+          )}>
           {item.name}
         </p>
-        <p className='text-h5 text-neutral-200 transition-colors duration-300 group-hover:text-neutral-600'>
+        <p
+          className={clsx(
+            'text-h5 text-neutral-200 transition-colors duration-300',
+            isActive ? 'text-neutral-600' : 'group-hover:text-neutral-600'
+          )}>
           {item.date}
         </p>
       </div>
