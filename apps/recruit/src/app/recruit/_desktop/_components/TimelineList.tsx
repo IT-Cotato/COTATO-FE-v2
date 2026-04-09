59ee115145980d707e3091313ebdd5ad32b6cf93
ion <delion.dev@gmail.com>
2026-04-09 11:17:44 +0900

    style(recruit): 데스크톱 TimelineList 컴포넌트 스타일 수정 (#340)

--- apps/recruit/src/app/recruit/_desktop/_components/TimelineList.tsx
+++ apps/recruit/src/app/recruit/_desktop/_components/TimelineList.tsx
@@ -10,8 +10,8 @@ export const TimelineList = ({timelines}: TimelineListProps) => {
     <div className='mb-30'>
       <p className='text-h2 mb-7.5 text-center text-neutral-800'>모집 일정</p>
 
-      <div className='flex w-full overflow-x-scroll [&::-webkit-scrollbar]:hidden'>
-        <div className='flex justify-center'>
+      <div className='w-full overflow-x-scroll [&::-webkit-scrollbar]:hidden'>
+        <div className='mx-auto flex w-max gap-10'>
           {timelines?.map((item, index) => (
             <TimelineItem
               key={item.title}
