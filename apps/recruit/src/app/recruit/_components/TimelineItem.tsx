e88dd0b4e139f8c5367958e742e2f01c733fd073
ion <delion.dev@gmail.com>
2026-04-09 11:16:00 +0900

    style(recruit): timeline item 컴포넌트 스타일 수정 (#340)

--- apps/recruit/src/app/recruit/_components/TimelineItem.tsx
+++ apps/recruit/src/app/recruit/_components/TimelineItem.tsx
@@ -10,10 +10,10 @@ interface TimelineItemProps {
 export const TimelineItem = ({item, isLast}: TimelineItemProps) => {
   return (
     <div className='flex shrink-0 select-none'>
-      <div className='flex w-41.5 flex-col items-center gap-3.5'>
+      <div className='flex flex-col items-center gap-3.5'>
         <div className='flex flex-col items-center gap-2.5'>
           <SmallLogo className='fill-hover h-7 w-7' />
-          <p className='text-body-l-sb text-center wrap-anywhere text-neutral-700'>
+          <p className='text-body-l-sb text-center whitespace-pre-line text-neutral-700'>
             {item.title}
           </p>
         </div>
