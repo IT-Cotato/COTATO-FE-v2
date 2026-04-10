820865c999b8301ed1e320501f6c638a2a063cf9
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-04-10 12:27:37 +0900

    refactor(ui): 패키지 내부 기존 컴포넌트들의 절대경로 참조를 상대경로로 롤백 (#337)

--- packages/ui/src/components/filter/CheckboxFilter.tsx
+++ packages/ui/src/components/filter/CheckboxFilter.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import {Checkbox} from '@repo/ui/components/checkbox/CheckBox';
+import {Checkbox} from '../checkbox/CheckBox';
 
 interface CheckboxFilterProps<T extends string> {
   options: T[];
